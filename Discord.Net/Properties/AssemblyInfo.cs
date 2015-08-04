ebd76f9ebef7b9b77b59e350b9e2c2dfcc12df20
Brandon Smith <brandon.smith@ironboots.net>
2015-08-04 14:36:58 -0300

    Updated AssemblyInfo

--- Discord.Net/Properties/AssemblyInfo.cs
+++ Discord.Net/Properties/AssemblyInfo.cs
@@ -1,36 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("DiscordAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Discord.Net")]
+[assembly: AssemblyDescription("A .Net API Wrapper for the Discord client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiscordAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("RogueException")]
+[assembly: AssemblyProduct("Discord.Net")]
+[assembly: AssemblyCopyright("Copyright © RogueException 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8d23f61b-723c-4966-859d-1119b28bcf19")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
