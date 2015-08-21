766b19bfd920393edb7e1e53c03ba8decb6944e0
Brandon Smith <brandon.smith@ironboots.net>
2015-08-21 18:17:04 -0300

    Added ChannelTypes documentation

--- src/Discord.Net/ChannelTypes.cs
+++ src/Discord.Net/ChannelTypes.cs
@@ -2,7 +2,9 @@
 {
 	public static class ChannelTypes
 	{
+		/// <summary> A text-only channel. </summary>
 		public const string Text = "text";
+		/// <summary> A voice-only channel. </summary>
 		public const string Voice = "voice";
 	}
 }
