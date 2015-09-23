d3b8b85b6f295e2c7b0934fb2fa114008e254679
RogueException <rogueexception@gmail.com>
2015-09-23 16:25:19 -0300

    Stop commiting broken builds >.>

--- src/Discord.Net/DiscordClient.API.cs
+++ src/Discord.Net/DiscordClient.API.cs
@@ -258,9 +258,9 @@ namespace Discord
 			{
 				int index = i * DiscordAPIClient.MaxMessageSize;
 				string blockText = text.Substring(index, Math.Min(2000, text.Length - index));
+				var nonce = GenerateNonce();
 				if (_config.UseMessageQueue)
 				{
-					var nonce = GenerateNonce();
 					var msg = _messages.GetOrAdd("nonce_" + nonce, channel.Id, _currentUserId);
 					var currentMember = _members[msg.UserId, channel.ServerId];
                     msg.Update(new Net.API.Message
