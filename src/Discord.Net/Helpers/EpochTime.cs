48da25752f117071bea17fd7a38db989d3a0b598
RogueException <rogueexception@gmail.com>
2015-10-17 00:20:34 -0300

    Made EpochTIme internal

--- src/Discord.Net/Helpers/EpochTime.cs
+++ src/Discord.Net/Helpers/EpochTime.cs
@@ -2,7 +2,7 @@
 
 namespace Discord.Helpers
 {
-    public class EpochTime
+    internal class EpochTime
 	{
 		private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 		public static ulong GetMilliseconds() => (ulong)(DateTime.UtcNow - epoch).TotalMilliseconds;
