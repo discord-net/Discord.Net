cfac08188418408c7dad33072cc38b2810c4a044
RogueException <rogueexception@gmail.com>
2015-10-18 08:30:19 -0300

    Renamed UserStatus.Away to Idle

--- src/Discord.Net/Enums/UserStatus.cs
+++ src/Discord.Net/Enums/UserStatus.cs
@@ -5,7 +5,7 @@
 		/// <summary> User is currently online and active. </summary>
 		public const string Online = "online";
 		/// <summary> User is currently online but inactive. </summary>
-		public const string Away = "away";
+		public const string Idle = "idle";
 		/// <summary> User is offline. </summary>
 		public const string Offline = "offline";
 	}
