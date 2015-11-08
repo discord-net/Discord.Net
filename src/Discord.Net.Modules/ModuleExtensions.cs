9bb77dd37352ab45349c746c3637a9e3214f7139
RogueException <rogueexception@gmail.com>
2015-11-08 12:43:14 -0400

    Added ModuleExtensions

---
+++ src/Discord.Net.Modules/ModuleExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Discord.Modules
+{
+    public static class ModuleExtensions
+    {
+		public static ModuleService Commands(this DiscordClient client)
+			=> client.GetService<ModuleService>();
+    }
+}
