50700c9943e4144aa2ddf9135e2eb6a909bd51fe
RogueException <rogueexception@gmail.com>
2015-11-27 04:35:01 -0400

    Moved libs from the lib folder to executable folder

--- src/Discord.Net/Audio/Sodium.cs
+++ src/Discord.Net/Audio/Sodium.cs
@@ -4,7 +4,7 @@ namespace Discord.Audio
 {
 	internal unsafe static class Sodium
 	{
-		[DllImport("lib/libsodium", EntryPoint = "crypto_secretbox_easy", CallingConvention = CallingConvention.Cdecl)]
+		[DllImport("libsodium", EntryPoint = "crypto_secretbox_easy", CallingConvention = CallingConvention.Cdecl)]
 		private static extern int SecretBoxEasy(byte* output, byte[] input, long inputLength, byte[] nonce, byte[] secret);
 
 		public static int Encrypt(byte[] input, long inputLength, byte[] output, int outputOffset, byte[] nonce, byte[] secret)
@@ -14,7 +14,7 @@ namespace Discord.Audio
 		}
 
 
-		[DllImport("lib/libsodium", EntryPoint = "crypto_secretbox_open_easy", CallingConvention = CallingConvention.Cdecl)]
+		[DllImport("libsodium", EntryPoint = "crypto_secretbox_open_easy", CallingConvention = CallingConvention.Cdecl)]
 		private static extern int SecretBoxOpenEasy(byte[] output, byte* input, long inputLength, byte[] nonce, byte[] secret);
 
 		public static int Decrypt(byte[] input, int inputOffset, long inputLength, byte[] output, byte[] nonce, byte[] secret)
