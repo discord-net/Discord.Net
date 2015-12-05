da75cebff2d4a2e6cd4ab99cffad629784bed887
RogueException <rogueexception@gmail.com>
2015-12-05 05:33:21 -0400

    Added missing files

---
+++ src/Discord.Net/Helpers/Shared/EventHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Discord
+{
+    public static class EventHelper
+	{
+		public static void Raise(Logger logger, string name, Action action)
+		{
+			try { action(); }
+			catch (Exception ex)
+			{
+				var ex2 = ex.GetBaseException();
+				logger.Log(LogSeverity.Error, $"{name}'s handler raised {ex2.GetType().Name}: ${ex2.Message}", ex);
+			}
+		}
+	}
+}
---
+++ src/Discord.Net/LogExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Discord
+{
+    public static class LogExtensions
+    {
+		public static LogService Log(this DiscordClient client, bool required = true)
+			=> client.GetService<LogService>(required);
+	}
+}
