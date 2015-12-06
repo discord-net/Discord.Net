0828d8eb4090de0cfa7ce84f66cec7f56bff5f70
RogueException <rogueexception@gmail.com>
2015-12-06 02:00:21 -0400

    Added Shared project, resync'd net45 projects with dnx ones

--- src/Discord.Net/Helpers/CollectionExtensions.cs
+++ src/Discord.Net/Helpers/CollectionExtensions.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Discord
 {
