5c18600a513ec799dc389bc7893f0a25a77048f8
RogueException <rogueexception@gmail.com>
2015-12-24 22:35:42 -0400

    Downgraded to Net45

--- src/Discord.Net/Logging/LogManager.cs
+++ src/Discord.Net/Logging/LogManager.cs
@@ -16,6 +16,7 @@ namespace Discord.Logging
             Level = client.Config.LogLevel;
         }
 
+#if !NET45
         public void Log(LogSeverity severity, string source, FormattableString message, Exception exception = null)
         {
             if (severity <= Level)
@@ -24,6 +25,7 @@ namespace Discord.Logging
                 catch { } //We dont want to log on log errors
             }
         }
+#endif
         public void Log(LogSeverity severity, string source, string message, Exception exception = null)
         {
             if (severity <= Level)
@@ -35,36 +37,46 @@ namespace Discord.Logging
 
         public void Error(string source, string message, Exception ex = null)
             => Log(LogSeverity.Error, source, message, ex);
+#if !NET45
         public void Error(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Error, source, message, ex);
+#endif
         public void Error(string source, Exception ex)
             => Log(LogSeverity.Error, source, (string)null, ex);
 
         public void Warning(string source, string message, Exception ex = null)
             => Log(LogSeverity.Warning, source, message, ex);
+#if !NET45
         public void Warning(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Warning, source, message, ex);
+#endif
         public void Warning(string source, Exception ex)
             => Log(LogSeverity.Warning, source, (string)null, ex);
 
         public void Info(string source, string message, Exception ex = null)
             => Log(LogSeverity.Info, source, message, ex);
+#if !NET45
         public void Info(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Info, source, message, ex);
+#endif
         public void Info(string source, Exception ex)
             => Log(LogSeverity.Info, source, (string)null, ex);
 
         public void Verbose(string source, string message, Exception ex = null)
             => Log(LogSeverity.Verbose, source, message, ex);
+#if !NET45
         public void Verbose(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Verbose, source, message, ex);
+#endif
         public void Verbose(string source, Exception ex)
             => Log(LogSeverity.Verbose, source, (string)null, ex);
 
         public void Debug(string source, string message, Exception ex = null)
             => Log(LogSeverity.Debug, source, message, ex);
+#if !NET45
         public void Debug(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Debug, source, message, ex);
+#endif
         public void Debug(string source, Exception ex)
             => Log(LogSeverity.Debug, source, (string)null, ex);
 
--- src/Discord.Net/Logging/Logger.cs
+++ src/Discord.Net/Logging/Logger.cs
@@ -17,41 +17,53 @@ namespace Discord.Logging
 
         public void Log(LogSeverity severity, string message, Exception exception = null)
             => _manager.Log(severity, Name, message, exception);
+#if !NET45
         public void Log(LogSeverity severity, FormattableString message, Exception exception = null)
             => _manager.Log(severity, Name, message, exception);
+#endif
 
         public void Error(string message, Exception exception = null)
             => _manager.Error(Name, message, exception);
+#if !NET45
         public void Error(FormattableString message, Exception exception = null)
             => _manager.Error(Name, message, exception);
+#endif
         public void Error(Exception exception)
             => _manager.Error(Name, exception);
 
         public void Warning(string message, Exception exception = null)
             => _manager.Warning(Name, message, exception);
+#if !NET45
         public void Warning(FormattableString message, Exception exception = null)
             => _manager.Warning(Name, message, exception);
+#endif
         public void Warning(Exception exception)
             => _manager.Warning(Name, exception);
 
         public void Info(string message, Exception exception = null)
             => _manager.Info(Name, message, exception);
+#if !NET45
         public void Info(FormattableString message, Exception exception = null)
             => _manager.Info(Name, message, exception);
+#endif
         public void Info(Exception exception)
             => _manager.Info(Name, exception);
 
         public void Verbose(string message, Exception exception = null)
             => _manager.Verbose(Name, message, exception);
+#if !NET45
         public void Verbose(FormattableString message, Exception exception = null)
             => _manager.Verbose(Name, message, exception);
+#endif
         public void Verbose(Exception exception)
             => _manager.Verbose(Name, exception);
 
         public void Debug(string message, Exception exception = null)
             => _manager.Debug(Name, message, exception);
+#if !NET45
         public void Debug(FormattableString message, Exception exception = null)
             => _manager.Debug(Name, message, exception);
+#endif
         public void Debug(Exception exception)
             => _manager.Debug(Name, exception);
     }
