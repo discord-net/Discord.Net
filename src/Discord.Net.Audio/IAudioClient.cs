4f7118f241d718867d66ae36384333b6e128429b
RogueException <rogueexception@gmail.com>
2016-01-05 00:17:23 -0400

    Updated IAudioClient

--- src/Discord.Net.Audio/IAudioClient.cs
+++ src/Discord.Net.Audio/IAudioClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 
 namespace Discord.Audio
 {
@@ -7,14 +8,16 @@ namespace Discord.Audio
         ConnectionState State { get; }
         Channel Channel { get; }
         Server Server { get; }
+        Stream OutputStream { get; }
 
         Task Join(Channel channel);
         Task Disconnect();
 
         /// <summary> Sends a PCM frame to the voice server. Will block until space frees up in the outgoing buffer. </summary>
         /// <param name="data">PCM frame to send. This must be a single or collection of uncompressed 48Kz monochannel 20ms PCM frames. </param>
+        /// <param name="offset">Offset . </param>
         /// <param name="count">Number of bytes in this frame. </param>
-        void Send(byte[] data, int count);
+        void Send(byte[] data, int offset, int count);
         /// <summary> Clears the PCM buffer. </summary>
         void Clear();
         /// <summary> Blocks until the voice output buffer is empty. </summary>
