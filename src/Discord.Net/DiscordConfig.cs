55769bea3ce785b74be8c1a3576e5003ab8ce645
RogueException <rogueexception@gmail.com>
2016-02-09 15:07:24 -0400

    Cleaned up DiscordConfig docstrings

--- src/Discord.Net/DiscordConfig.cs
+++ src/Discord.Net/DiscordConfig.cs
@@ -29,29 +29,29 @@ namespace Discord
 
         //Global
 
-        /// <summary> Name of your application. This is used both for the token cache directory and user agent. </summary>
+        /// <summary> Gets or sets name of your application, used both for the token cache directory and user agent. </summary>
         public string AppName { get { return _appName; } set { SetValue(ref _appName, value); UpdateUserAgent(); } }
         private string _appName = null;
-        /// <summary> Url to your application. This is used in the user agent. </summary>
+        /// <summary> Gets or sets url to your application, used in the user agent. </summary>
         public string AppUrl { get { return _appUrl; } set { SetValue(ref _appUrl, value); UpdateUserAgent(); } }
         private string _appUrl = null;
-        /// <summary> Version of your application. </summary>
+        /// <summary> Gets or sets the version of your application, used in the user agent. </summary>
         public string AppVersion { get { return _appVersion; } set { SetValue(ref _appVersion, value); UpdateUserAgent(); } }
         private string _appVersion = null;
 
-        /// <summary> Specifies the minimum log level severity that will be sent to the LogMessage event. Warning: setting this to debug will really hurt performance but should help investigate any internal issues. </summary>
+        /// <summary> Gets or sets the minimum log level severity that will be sent to the LogMessage event. Warning: setting this to debug will really hurt performance but should help investigate any internal issues. </summary>
         public LogSeverity LogLevel { get { return _logLevel; } set { SetValue(ref _logLevel, value); } }
         private LogSeverity _logLevel = LogSeverity.Info;
         /// <summary> Enables or disables the default event logger. </summary>
         public bool LogEvents { get { return _logEvents; } set { SetValue(ref _logEvents, value); } }
         private bool _logEvents = true;
 
-        /// <summary> User Agent string to use when connecting to Discord. </summary>
+        /// <summary> Gets the user agent used when connecting to Discord. </summary>
         public string UserAgent { get; private set; }
 
         //Rest
 
-        /// <summary> Max time (in milliseconds) to wait for an API request to complete. </summary>
+        /// <summary> Gets or sets the max time (in milliseconds) to wait for an API request to complete. </summary>
         public int RestTimeout { get { return _restTimeout; } set { SetValue(ref _restTimeout, value); } }
 		private int _restTimeout = 10000;
 
@@ -83,7 +83,7 @@ namespace Discord
         /// <summary> Cache an encrypted login token to temp dir after success login. </summary>
         public bool CacheToken { get { return _cacheToken; } set { SetValue(ref _cacheToken, value); } }
         private bool _cacheToken = true;
-        /// <summary> Instructs Discord to not send send information about offline users, for servers with more than 50 users. </summary>
+        /// <summary> Gets or sets whether Discord should send information about offline users, for servers with more than 100 users. </summary>
         public bool UseLargeThreshold { get { return _useLargeThreshold; } set { SetValue(ref _useLargeThreshold, value); } }
         private bool _useLargeThreshold = false;
         /// <summary> Gets or sets the number of messages per channel that should be kept in cache. Setting this to zero disables the message cache entirely. </summary>
