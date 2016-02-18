3b71a329b823fe0f74ba96390d41f28d47f452b1
RogueException <rogueexception@gmail.com>
2016-02-18 19:40:46 -0400

    AudioExtensions.GetAudioClient should be an extension method

--- src/Discord.Net.Audio/AudioExtensions.cs
+++ src/Discord.Net.Audio/AudioExtensions.cs
@@ -21,6 +21,6 @@ namespace Discord.Audio
         public static Task<IAudioClient> JoinAudio(this Channel channel) => channel.Client.GetService<AudioService>().Join(channel);
         public static Task LeaveAudio(this Channel channel) => channel.Client.GetService<AudioService>().Leave(channel);
         public static Task LeaveAudio(this Server server) => server.Client.GetService<AudioService>().Leave(server);
-        public static IAudioClient GetAudioClient(Server server) => server.Client.GetService<AudioService>().GetClient(server);
+        public static IAudioClient GetAudioClient(this Server server) => server.Client.GetService<AudioService>().GetClient(server);
     }
 }
