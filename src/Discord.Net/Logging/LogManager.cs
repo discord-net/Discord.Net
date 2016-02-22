1ef5e4a1ca2cadd8cc1dc935c1c47fbee5ce7d40
RogueException <rogueexception@gmail.com>
2016-02-22 00:22:26 -0400

    Cleaned up LogManager

--- src/Discord.Net/Logging/LogManager.cs
+++ src/Discord.Net/Logging/LogManager.cs
@@ -16,6 +16,15 @@ namespace Discord.Logging
             Level = client.Config.LogLevel;
         }
 
+        public void Log(LogSeverity severity, string source, string message, Exception exception = null)
+        {
+            if (severity <= Level)
+            {
+                try { Message(this, new LogMessageEventArgs(severity, source, message, exception)); }
+                catch { } //We dont want to log on log errors
+            }
+        }
+
 #if DOTNET5_4
         public void Log(LogSeverity severity, string source, FormattableString message, Exception exception = null)
         {
@@ -26,59 +35,40 @@ namespace Discord.Logging
             }
         }
 #endif
-        public void Log(LogSeverity severity, string source, string message, Exception exception = null)
-        {
-            if (severity <= Level)
-            {
-                try { Message(this, new LogMessageEventArgs(severity, source, message, exception)); }
-                catch { } //We dont want to log on log errors
-            }
-        }
 
         public void Error(string source, string message, Exception ex = null)
             => Log(LogSeverity.Error, source, message, ex);
+        public void Error(string source, Exception ex)
+            => Log(LogSeverity.Error, source, (string)null, ex);
+        public void Warning(string source, string message, Exception ex = null)
+            => Log(LogSeverity.Warning, source, message, ex);
+        public void Warning(string source, Exception ex)
+            => Log(LogSeverity.Warning, source, (string)null, ex);
+        public void Info(string source, string message, Exception ex = null)
+            => Log(LogSeverity.Info, source, message, ex);
+        public void Info(string source, Exception ex)
+            => Log(LogSeverity.Info, source, (string)null, ex);
+        public void Verbose(string source, string message, Exception ex = null)
+            => Log(LogSeverity.Verbose, source, message, ex);
+        public void Verbose(string source, Exception ex)
+            => Log(LogSeverity.Verbose, source, (string)null, ex);
+        public void Debug(string source, string message, Exception ex = null)
+            => Log(LogSeverity.Debug, source, message, ex);
+        public void Debug(string source, Exception ex)
+            => Log(LogSeverity.Debug, source, (string)null, ex);
+
 #if DOTNET5_4
         public void Error(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Error, source, message, ex);
-#endif
-        public void Error(string source, Exception ex)
-            => Log(LogSeverity.Error, source, (string)null, ex);
-
-        public void Warning(string source, string message, Exception ex = null)
-            => Log(LogSeverity.Warning, source, message, ex);
-#if DOTNET5_4
         public void Warning(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Warning, source, message, ex);
-#endif
-        public void Warning(string source, Exception ex)
-            => Log(LogSeverity.Warning, source, (string)null, ex);
-
-        public void Info(string source, string message, Exception ex = null)
-            => Log(LogSeverity.Info, source, message, ex);
-#if DOTNET5_4
         public void Info(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Info, source, message, ex);
-#endif
-        public void Info(string source, Exception ex)
-            => Log(LogSeverity.Info, source, (string)null, ex);
-
-        public void Verbose(string source, string message, Exception ex = null)
-            => Log(LogSeverity.Verbose, source, message, ex);
-#if DOTNET5_4
         public void Verbose(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Verbose, source, message, ex);
-#endif
-        public void Verbose(string source, Exception ex)
-            => Log(LogSeverity.Verbose, source, (string)null, ex);
-
-        public void Debug(string source, string message, Exception ex = null)
-            => Log(LogSeverity.Debug, source, message, ex);
-#if DOTNET5_4
         public void Debug(string source, FormattableString message, Exception ex = null)
             => Log(LogSeverity.Debug, source, message, ex);
 #endif
-        public void Debug(string source, Exception ex)
-            => Log(LogSeverity.Debug, source, (string)null, ex);
 
         public Logger CreateLogger(string name) => new Logger(this, name);
     }
