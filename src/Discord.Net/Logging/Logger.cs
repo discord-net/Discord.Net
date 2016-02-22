c387e04c09a70de3a3f8935bbf7766ff204f31c9
RogueException <rogueexception@gmail.com>
2016-02-21 23:09:36 -0400

    Reorganize Logger.cs

--- src/Discord.Net/Logging/Logger.cs
+++ src/Discord.Net/Logging/Logger.cs
@@ -17,54 +17,40 @@ namespace Discord.Logging
 
         public void Log(LogSeverity severity, string message, Exception exception = null)
             => _manager.Log(severity, Name, message, exception);
+        public void Error(string message, Exception exception = null)
+            => _manager.Error(Name, message, exception);
+        public void Error(Exception exception)
+            => _manager.Error(Name, exception);
+        public void Warning(string message, Exception exception = null)
+            => _manager.Warning(Name, message, exception);
+        public void Warning(Exception exception)
+            => _manager.Warning(Name, exception);
+        public void Info(string message, Exception exception = null)
+            => _manager.Info(Name, message, exception);
+        public void Info(Exception exception)
+            => _manager.Info(Name, exception);
+        public void Verbose(string message, Exception exception = null)
+            => _manager.Verbose(Name, message, exception);
+        public void Verbose(Exception exception)
+            => _manager.Verbose(Name, exception);
+        public void Debug(string message, Exception exception = null)
+            => _manager.Debug(Name, message, exception);
+        public void Debug(Exception exception)
+            => _manager.Debug(Name, exception);
+
 #if DOTNET5_4
         public void Log(LogSeverity severity, FormattableString message, Exception exception = null)
             => _manager.Log(severity, Name, message, exception);
-#endif
-
-        public void Error(string message, Exception exception = null)
-            => _manager.Error(Name, message, exception);
-#if DOTNET5_4
         public void Error(FormattableString message, Exception exception = null)
             => _manager.Error(Name, message, exception);
-#endif
-        public void Error(Exception exception)
-            => _manager.Error(Name, exception);
-
-        public void Warning(string message, Exception exception = null)
-            => _manager.Warning(Name, message, exception);
-#if DOTNET5_4
         public void Warning(FormattableString message, Exception exception = null)
             => _manager.Warning(Name, message, exception);
-#endif
-        public void Warning(Exception exception)
-            => _manager.Warning(Name, exception);
-
-        public void Info(string message, Exception exception = null)
-            => _manager.Info(Name, message, exception);
-#if DOTNET5_4
         public void Info(FormattableString message, Exception exception = null)
             => _manager.Info(Name, message, exception);
-#endif
-        public void Info(Exception exception)
-            => _manager.Info(Name, exception);
-
-        public void Verbose(string message, Exception exception = null)
-            => _manager.Verbose(Name, message, exception);
-#if DOTNET5_4
         public void Verbose(FormattableString message, Exception exception = null)
             => _manager.Verbose(Name, message, exception);
-#endif
-        public void Verbose(Exception exception)
-            => _manager.Verbose(Name, exception);
-
-        public void Debug(string message, Exception exception = null)
-            => _manager.Debug(Name, message, exception);
-#if DOTNET5_4
         public void Debug(FormattableString message, Exception exception = null)
             => _manager.Debug(Name, message, exception);
 #endif
-        public void Debug(Exception exception)
-            => _manager.Debug(Name, exception);
     }
 }
