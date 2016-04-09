ac66f59d4bc660357e09645801ae68285b03b1a5
RogueException <rogueexception@gmail.com>
2016-04-09 11:34:51 -0300

    Updated Role Model

--- src/Discord.Net/API/Common/Role.cs
+++ src/Discord.Net/API/Common/Role.cs
@@ -6,16 +6,16 @@ namespace Discord.API
     {
         [JsonProperty("id")]
         public ulong Id { get; set; }
-        [JsonProperty("permissions")]
-        public uint? Permissions { get; set; }
         [JsonProperty("name")]
         public string Name { get; set; }
-        [JsonProperty("position")]
-        public int? Position { get; set; }
-        [JsonProperty("hoist")]
-        public bool? Hoist { get; set; }
         [JsonProperty("color")]
         public uint? Color { get; set; }
+        [JsonProperty("hoist")]
+        public bool? Hoist { get; set; }
+        [JsonProperty("position")]
+        public int? Position { get; set; }
+        [JsonProperty("permissions")]
+        public uint? Permissions { get; set; }
         [JsonProperty("managed")]
         public bool? Managed { get; set; }
     }
