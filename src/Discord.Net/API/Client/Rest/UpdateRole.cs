549b32de6d7aa5be4b0245d2ce06aef8adaf2d57
Lirusaito <inusaito@gmail.com>
2016-04-30 11:37:04 -0400

    Support Role Mentions
    `Message.MentionedRoles` is now populated properly with roles.
    `Role.IsMentionable` has been added to determine whether
    `Role.Mention` now mentions roles that are `IsMentionable` as expected (otherwise returns empty string)
    `Role.Edit` now takes parameter `bool? isMentionable`
    `Server.CreateRole` now takes parameter `bool isMentionable`
    `IsMentionable` added to `UpdateRoleRequest`

--- src/Discord.Net/API/Client/Rest/UpdateRole.cs
+++ src/Discord.Net/API/Client/Rest/UpdateRole.cs
@@ -18,6 +18,8 @@ namespace Discord.API.Client.Rest
         public uint Permissions { get; set; }
         [JsonProperty("hoist")]
         public bool IsHoisted { get; set; }
+        [JsonProperty("mentionable")]
+        public bool IsMentionable { get; set; }
         [JsonProperty("color")]
         public uint Color { get; set; }
 
