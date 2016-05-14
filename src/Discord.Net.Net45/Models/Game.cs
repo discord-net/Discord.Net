234185b60315a0fe0db1c2512bdd64751cf21b49
Khionu Terabite <abstractkitty@iamtrue.net>
2016-05-14 19:37:48 -0400

    Confirmed working now

--- src/Discord.Net.Net45/Models/Game.cs
+++ src/Discord.Net.Net45/Models/Game.cs
@@ -1,9 +1,14 @@
-﻿namespace Discord
+﻿using Newtonsoft.Json;
+
+namespace Discord
 {
     public class GameInfo
     {
+        [JsonProperty("game")]
         public string Name { get; set; }
+        [JsonProperty("url")]
         public string Url { get; set; }
+        [JsonProperty("type")]
         public GameType Type { get; set; }
     }
 }
