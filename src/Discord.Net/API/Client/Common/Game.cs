41629525e852e24e95650e14a92dc0464fc3ad5c
RogueException <rogueexception@gmail.com>
2016-05-18 19:25:22 -0300

    Fixed game name not serializing

--- src/Discord.Net/API/Client/Common/Game.cs
+++ src/Discord.Net/API/Client/Common/Game.cs
@@ -4,7 +4,7 @@ namespace Discord.API.Client
 {
     public class Game
     {
-        [JsonProperty("game")]
+        [JsonProperty("name")]
         public string Name { get; set; }
         [JsonProperty("url")]
         public string Url { get; set; }
