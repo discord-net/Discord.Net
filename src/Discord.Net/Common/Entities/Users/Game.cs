4aba51f2fdb7b65c8c76c9e30b62aa90207df476
RogueException <rogueexception@gmail.com>
2016-05-19 00:06:12 -0300

    Added Game(name) constructor

--- src/Discord.Net/Common/Entities/Users/Game.cs
+++ src/Discord.Net/Common/Entities/Users/Game.cs
@@ -6,6 +6,12 @@
         public string StreamUrl { get; }
         public StreamType StreamType { get; }
 
+        public Game(string name)
+        {
+            Name = name;
+            StreamUrl = null;
+            StreamType = StreamType.NotStreaming;
+        }
         public Game(string name, string streamUrl, StreamType type)
         {
             Name = name;
