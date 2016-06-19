f24fa964aef31ba069c0d274ac4d0e2862812779
RogueException <rogueexception@gmail.com>
2016-06-19 15:15:36 -0300

    Added ToString and DebuggerDisplay to Game

--- src/Discord.Net/Entities/Users/Game.cs
+++ src/Discord.Net/Entities/Users/Game.cs
@@ -1,7 +1,9 @@
-﻿using Model = Discord.API.Game;
+﻿using System.Diagnostics;
+using Model = Discord.API.Game;
 
 namespace Discord
 {
+    [DebuggerDisplay(@"{DebuggerDisplay,nq}")]
     public struct Game
     {
         public string Name { get; }
@@ -18,5 +20,8 @@ namespace Discord
             : this(name, null, StreamType.NotStreaming) { }
         internal Game(Model model)
             : this(model.Name, model.StreamUrl.GetValueOrDefault(null), model.StreamType.GetValueOrDefault(null) ?? StreamType.NotStreaming) { }
+
+        public override string ToString() => Name;
+        private string DebuggerDisplay => StreamUrl != null ? $"{Name} ({StreamUrl})" : Name;
     }
 }
