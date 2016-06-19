480034feedf762d4bd808fb1e09a941e97893c14
RogueException <rogueexception@gmail.com>
2016-06-19 18:55:12 -0300

    Added command map

---
+++ src/Discord.Net.Commands/CommandSearchResults.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Discord.Commands
+{
+    public struct CommandSearchResults
+    {
+        IReadOnlyList<Command> Commands { get; }
+        int ArgsPos { get; }
+
+        public CommandSearchResults(IReadOnlyList<Command> commands, int argsPos)
+        {
+            Commands = commands;
+            ArgsPos = argsPos;
+        }
+    }
+}
