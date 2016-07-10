c72bf0678bd47cdf48b9a41dedc589e3e8534d33
Khionu Terabite <khionu@users.noreply.github.com>
2016-07-10 01:31:10 -0400

    Added ToString and DebuggerDisplay
    Casting a hex :ok-hand:

--- src/Discord.Net/Entities/Roles/Color.cs
+++ src/Discord.Net/Entities/Roles/Color.cs
@@ -32,6 +32,11 @@
                 ((uint)(r * 255.0f) << 16) |
                 ((uint)(g * 255.0f) << 8) |
                 (uint)(b * 255.0f);
-        }        
+        }
+        
+        public override string ToString() =>
+            $"#{Convert.ToString(RawValue, 16)}";
+        private string DebuggerDisplay() =>
+            $"#{Convert.ToString(RawValue, 16)} ({RawValue})";
     }
 }
