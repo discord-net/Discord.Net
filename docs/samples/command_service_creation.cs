9dae654096ad4a0dda0279e9cfd4e6df8d73d6c1
Christopher F <foxbot@protonmail.com>
2016-07-21 16:59:37 -0400

    Remove RTD docs

--- docs/samples/command_service_creation.cs
+++
@@ -1,9 +0,0 @@
-//create command service
-var commandService = new CommandService(new CommandServiceConfig
-{
-    CommandChar = '~', // prefix char for commands
-    HelpMode = HelpMode.Public
-});
-
-//add command service
-var commands = client.AddService(commandService);
