848ffc71adcc3e58cc222b6742a0e22dba0bd48e
RogueException <rogueexception@gmail.com>
2016-07-29 16:30:55 -0300

    Cleaned up DiscordConfig.Version

--- src/Discord.Net/DiscordConfig.cs
+++ src/Discord.Net/DiscordConfig.cs
@@ -5,7 +5,10 @@ namespace Discord
     public class DiscordConfig
     {
         public const int APIVersion = 6;        
-        public static string Version { get; } = typeof(DiscordRestConfig).GetTypeInfo().Assembly?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "Unknown";
+        public static string Version { get; } =
+            typeof(DiscordConfig).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ??
+            typeof(DiscordConfig).GetTypeInfo().Assembly.GetName().Version.ToString(3) ?? 
+            "Unknown";
 
         public static readonly string ClientAPIUrl = $"https://discordapp.com/api/v{APIVersion}/";
         public const string CDNUrl = "https://cdn.discordapp.com/";
