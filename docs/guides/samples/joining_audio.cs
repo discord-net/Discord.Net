c6115ea7f6fab352d621311b5a6f6ef28ddc6342
Christopher F <foxbot@protonmail.com>
2016-08-01 21:05:27 -0400

    [docs] Document TypeReaders, Events, and Joining Audio

---
+++ docs/guides/samples/joining_audio.cs
@@ -0,0 +1,15 @@
+// Create an IAudioClient, and store it for later use
+private IAudioClient _audio;
+
+// Create a Join command, that will join the parameter or the user's current voice channel
+[Command("join")]
+public async Task JoinChannel(IMessage msg,
+    IVoiceChannel channel = null)
+{
+    // Get the audio channel
+    channel = channel ?? (msg.Author as IGuildUser)?.VoiceChannel;
+    if (channel == null) { await msg.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+    // Get the IAudioClient by calling the JoinAsync method
+    _audio = await channel.JoinAsync();
+}
