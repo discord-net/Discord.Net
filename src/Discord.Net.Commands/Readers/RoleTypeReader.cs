2cd97bd5946398692d80708f2162f47020aa318f
Master Kwoth <Kwoth@users.noreply.github.com>
2016-08-27 03:27:10 +0200

    Fixed IRole typereader ref #229

--- src/Discord.Net.Commands/Readers/RoleTypeReader.cs
+++ src/Discord.Net.Commands/Readers/RoleTypeReader.cs
@@ -32,7 +32,7 @@ namespace Discord.Commands
                     AddResult(results, role as T, role.Name == input ? 0.80f : 0.70f);
 
                 if (results.Count > 0)
-                    return Task.FromResult(TypeReaderResult.FromSuccess(results));
+                    return Task.FromResult(TypeReaderResult.FromSuccess(results.Values));
             }
             return Task.FromResult(TypeReaderResult.FromError(CommandError.ObjectNotFound, "Role not found."));
         }
