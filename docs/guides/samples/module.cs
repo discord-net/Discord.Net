a2b4f47acfdfd11073f11e5dfc22e9f61fc18c99
Christopher F <foxbot@protonmail.com>
2016-09-10 14:26:24 -0400

    [docs] replace description with summary
    Resolves #280

--- docs/guides/samples/module.cs
+++ docs/guides/samples/module.cs
@@ -6,9 +6,9 @@ using Discord.WebSocket;
 public class Info
 {
     // ~say hello -> hello
-    [Command("say"), Description("Echos a message.")]
+    [Command("say"), Summary("Echos a message.")]
     public async Task Say(IUserMessage msg,
-        [Unparsed, Description("The text to echo")] string echo)
+        [Unparsed, Summary("The text to echo")] string echo)
     {
         await msg.Channel.SendMessageAsync(echo);
     }
@@ -19,9 +19,9 @@ public class Info
 public class Sample
 {
     // ~sample square 20 -> 
-    [Command("square"), Description("Squares a number.")]
+    [Command("square"), Summary("Squares a number.")]
     public async Task Square(IUserMessage msg,
-        [Description("The number to square.")] int num)
+        [Summary("The number to square.")] int num)
     {
         await msg.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
     }
@@ -32,10 +32,10 @@ public class Sample
 	// ~sample userinfo Khionu --> Khionu#8708
 	// ~sample userinfo 96642168176807936 --> Khionu#8708
     // ~sample whois 96642168176807936 --> Khionu#8708
-	[Command("userinfo"), Description("Returns info about the current user, or the user parameter, if one passed.")]
+	[Command("userinfo"), Summary("Returns info about the current user, or the user parameter, if one passed.")]
     [Alias("user", "whois")]
     public async Task UserInfo(IUserMessage msg,
-        [Description("The (optional) user to get info for")] IUser user = null)
+        [Summary("The (optional) user to get info for")] IUser user = null)
     {
         var userInfo = user ?? await Program.Client.GetCurrentUserAsync();
         await msg.Channel.SendMessageAsync($"{userInfo.Username}#{userInfo.Discriminator}");
