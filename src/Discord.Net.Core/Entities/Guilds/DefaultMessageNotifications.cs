8be5b3697dab33698903d08477deea5e47d930a1
RogueException <rogueexception@gmail.com>
2016-10-05 01:59:39 -0300

    Fixed DefaultMessageNotifs enum

--- src/Discord.Net.Core/Entities/Guilds/DefaultMessageNotifications.cs
+++ src/Discord.Net.Core/Entities/Guilds/DefaultMessageNotifications.cs
@@ -2,9 +2,9 @@
 {
     public enum DefaultMessageNotifications
     {
-        /// <summary> By default, only mentions will trigger notifications. </summary>
-        MentionsOnly = 0,
         /// <summary> By default, all messages will trigger notifications. </summary>
-        AllMessages = 1
+        AllMessages = 0,
+        /// <summary> By default, only mentions will trigger notifications. </summary>
+        MentionsOnly = 1
     }
 }
