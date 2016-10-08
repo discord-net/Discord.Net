82429fdcd177e0c595f569642d65ce48c28f96eb
RogueException <rogueexception@gmail.com>
2016-10-08 16:10:47 -0300

    Removed unneeded attribute

--- src/Discord.Net.Core/API/Rest/CreateGuildBanParams.cs
+++ src/Discord.Net.Core/API/Rest/CreateGuildBanParams.cs
@@ -1,9 +1,6 @@
 ﻿#pragma warning disable CS1591
-using Newtonsoft.Json;
-
 namespace Discord.API.Rest
 {
-    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     public class CreateGuildBanParams
     {
         public Optional<int> DeleteMessageDays { get; set; }
