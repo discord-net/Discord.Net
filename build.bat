2d3e2c46aeb4b7347de4b7706707d9befa030b50
RogueException <rogueexception@gmail.com>
2016-10-12 02:29:52 -0300

    Updated build script

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 @echo Off
 dotnet restore
 dotnet pack "src\Discord.Net" -c "%Configuration%" -o "artifacts"
+dotnet pack "src\Discord.Net.Core" -c "%Configuration%" -o "artifacts"
+dotnet pack "src\Discord.Net.Rest" -c "%Configuration%" -o "artifacts"
+dotnet pack "src\Discord.Net.WebSocket" -c "%Configuration%" -o "artifacts"
+dotnet pack "src\Discord.Net.Rpc" -c "%Configuration%" -o "artifacts"
 dotnet pack "src\Discord.Net.Commands" -c "%Configuration%" -o "artifacts"
