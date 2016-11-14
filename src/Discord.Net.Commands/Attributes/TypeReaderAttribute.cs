546831f72c757be52e263e5fe6bbb1561285942a
Joe4evr <jii.geugten@gmail.com>
2016-11-14 21:25:54 +0100

    Added doc comments.

--- src/Discord.Net.Commands/Attributes/TypeReaderAttribute.cs
+++ src/Discord.Net.Commands/Attributes/TypeReaderAttribute.cs
@@ -3,12 +3,27 @@ using System.Reflection;
 
 namespace Discord.Commands
 {
+    /// <summary>
+    /// Allows to override the <see cref="TypeReader"/> used for a parameter/type.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = true)]
     public class TypeReaderAttribute : Attribute
     {
+        /// <summary>
+        /// Type of the type that is read.
+        /// </summary>
         public Type Type { get; }
+
+        /// <summary>
+        /// <see cref="TypeInfo"/> of the specified <see cref="TypeReader"/>.
+        /// </summary>
         public TypeInfo OverridingTypeReader { get; }
 
+        /// <summary>
+        /// Specify a <see cref="TypeReader"/> for a particular type.
+        /// </summary>
+        /// <param name="forType">Type of the type to read.</param>
+        /// <param name="typeReader">Type of the <see cref="TypeReader"/> that reads the type.</param>
         public TypeReaderAttribute(Type forType, Type typeReader)
         {
             if (!typeof(TypeReader).GetTypeInfo().IsAssignableFrom(typeReader.GetTypeInfo()))
