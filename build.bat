42cbd67ee0a685cb49940dca64695d6a7cd3ea9c
RogueException <rogueexception@gmail.com>
2016-11-17 16:23:12 -0400

    Removed prerelease tag from project.json

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 @echo Off
 dotnet restore
 dotnet build
-dotnet pack "src\Discord.Net" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
-dotnet pack "src\Discord.Net.Core" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
-dotnet pack "src\Discord.Net.Commands" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
-dotnet pack "src\Discord.Net.Rest" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
-dotnet pack "src\Discord.Net.WebSocket" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
-dotnet pack "src\Discord.Net.Rpc" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
+dotnet pack "src\Discord.Net" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%" --no-build
+dotnet pack "src\Discord.Net.Core" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%" --no-build
+dotnet pack "src\Discord.Net.Commands" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%" --no-build
+dotnet pack "src\Discord.Net.Rest" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%" --no-build
+dotnet pack "src\Discord.Net.WebSocket" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%" --no-build
+dotnet pack "src\Discord.Net.Rpc" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%" --no-build
 
 REM dotnet pack "src\Discord.Net\Discord.Net.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
 REM dotnet pack "src\Discord.Net.Core\Discord.Net.Core.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%BuildCounter%" --no-build
