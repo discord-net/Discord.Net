e6cfd676648dcdb3e8a6c7fd19fde0900de1587f
Aaron Scherer <aequasi@gmail.com>
2016-11-16 20:02:32 -0800

    Unbreaking enum reader

--- src/Discord.Net.Commands/Readers/EnumTypeReader.cs
+++ src/Discord.Net.Commands/Readers/EnumTypeReader.cs
@@ -32,12 +32,10 @@ namespace Discord.Commands
             var byNameBuilder = ImmutableDictionary.CreateBuilder<string, object>();
             var byValueBuilder = ImmutableDictionary.CreateBuilder<T, object>();
             
-            foreach (var v in Enum.GetNames(_enumType))
+            foreach (var v in Enum.GetValues(_enumType))
             {
-                byNameBuilder.Add(v.ToLower(), v);
-                var parsedValue = (T)Enum.Parse(_enumType, v);
-                if (!byValueBuilder.ContainsKey(parsedValue))
-                    byValueBuilder.Add(parsedValue, v);
+                byNameBuilder.Add(v.ToString().ToLower(), v);
+                byValueBuilder.Add((T)v, v);
             }
 
             _enumsByName = byNameBuilder.ToImmutable();
