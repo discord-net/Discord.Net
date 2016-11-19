d2d7b4dce7bb578eefbf078428f1ae0b34f33437
FiniteReality <FiniteReality@users.noreply.github.com>
2016-11-19 21:16:28 +0000

    Make `_typeReaderTypeInfo` static
    Seems I missed this originally, whoops.

--- src/Discord.Net.Commands/Attributes/OverrideTypeReaderAttribute.cs
+++ src/Discord.Net.Commands/Attributes/OverrideTypeReaderAttribute.cs
@@ -7,7 +7,7 @@ namespace Discord.Commands
     [AttributeUsage(AttributeTargets.Parameter)]
     public class OverrideTypeReaderAttribute : Attribute
     {
-        private readonly TypeInfo _typeReaderTypeInfo = typeof(TypeReader).GetTypeInfo();
+        private static readonly TypeInfo _typeReaderTypeInfo = typeof(TypeReader).GetTypeInfo();
 
         public Type TypeReader { get; }
 
