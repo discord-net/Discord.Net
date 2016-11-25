fdecfe6bd4adcd0fc1d01948cf996ac0c8039eac
Christopher F <foxbot@protonmail.com>
2016-11-25 17:44:37 -0500

    Add RequireOwner Precondition
    This precondition will require that the invoker of the command is the owner of the bot.

---
+++ src/Discord.Net.Commands/Attributes/Preconditions/RequireOwnerAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Discord.Commands
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class RequireOwnerAttribute : PreconditionAttribute
+    {
+        private IApplication application;
+
+        public override async Task<PreconditionResult> CheckPermissions(CommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            if (application == null)
+                application = await context.Client.GetApplicationInfoAsync();
+            if (context.User.Id == application.Owner.Id) return PreconditionResult.FromSuccess();
+            return PreconditionResult.FromError("Command can only be run by the owner of the bot");
+        }
+    }
+}
