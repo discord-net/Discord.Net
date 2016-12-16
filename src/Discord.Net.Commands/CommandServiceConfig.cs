2dd62ba5e70d34d5c05d89b7e2944a67597267ff
RogueException <rogueexception@gmail.com>
2016-12-16 09:15:32 -0400

    Set DefaultRunMode to Sync

--- src/Discord.Net.Commands/CommandServiceConfig.cs
+++ src/Discord.Net.Commands/CommandServiceConfig.cs
@@ -3,7 +3,7 @@
     public class CommandServiceConfig
     {
         /// <summary> The default RunMode commands should have, if one is not specified on the Command attribute or builder. </summary>
-        public RunMode DefaultRunMode { get; set; } = RunMode.Mixed;
+        public RunMode DefaultRunMode { get; set; } = RunMode.Sync;
         /// <summary> Should commands be case-sensitive? </summary>
         public bool CaseSensitiveCommands { get; set; } = false;
     }
