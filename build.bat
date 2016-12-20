1fd9452e6c4e44612b05082524f7304babbb2766
RogueException <rogueexception@gmail.com>
2016-12-20 09:41:42 -0400

    Added WS4Net provider to build script

--- build.bat
+++ build.bat
@@ -6,6 +6,7 @@ dotnet pack "src\Discord.Net.Commands" -c "%Configuration%" -o "artifacts" --ver
 dotnet pack "src\Discord.Net.Rest" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
 dotnet pack "src\Discord.Net.WebSocket" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
 dotnet pack "src\Discord.Net.Rpc" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
+dotnet pack "src\Discord.Net.Providers.WS4Net" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
 
 REM dotnet pack "src\Discord.Net\Discord.Net.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
 REM dotnet pack "src\Discord.Net.Core\Discord.Net.Core.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
@@ -13,3 +14,4 @@ REM dotnet pack "src\Discord.Net.Commands\Discord.Net.Commands.csproj" -c "%Conf
 REM dotnet pack "src\Discord.Net.Rest\Discord.Net.Rest.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
 REM dotnet pack "src\Discord.Net.WebSocket\Discord.Net.WebSocket.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
 REM dotnet pack "src\Discord.Net.Rpc\Discord.Net.Rpc.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
+REM dotnet pack "src\Discord.Net.Providers.WS4Net\Discord.Net.Providers.WS4Net.csproj" -c "%Configuration%" -o "artifacts" --version-suffix "%PrereleaseTag%"
