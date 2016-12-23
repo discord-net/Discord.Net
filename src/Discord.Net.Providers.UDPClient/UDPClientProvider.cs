ca6eb6aff40d22b15b80ad53b54d25e8b955fc05
RogueException <rogueexception@gmail.com>
2016-12-23 15:07:59 -0400

    Added UDPClient Provider

---
+++ src/Discord.Net.Providers.UDPClient/UDPClientProvider.cs
@@ -0,0 +1,9 @@
+﻿using Discord.Net.Udp;
+
+namespace Discord.Net.Providers.UDPClient
+{
+    public static class UDPClientProvider
+    {
+        public static readonly UdpSocketProvider Instance = () => new UDPClient();
+    }
+}
