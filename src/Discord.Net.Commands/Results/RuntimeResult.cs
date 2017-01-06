c76ffdc8983a29300d9d762e0153ada58bc65b86
FiniteReality <FiniteReality@users.noreply.github.com>
2017-01-06 14:37:34 +0000

    Allow commands to return a Task<RuntimeResult>
    This allows bot developers to centralize command result logic by
    using result data whether the command as successful or not.
    
    Example usage:
    ```csharp
    var _result = await Commands.ExecuteAsync(context, argPos);
    if (_result is RuntimeResult result)
    {
        await message.Channel.SendMessageAsync(result.Reason);
    }
    else if (!_result.IsSuccess)
    {
        // Previous error handling
    }
    ```
    
    The RuntimeResult class can be subclassed too, for example:
    ```csharp
    var _result = await Commands.ExecuteAsync(context, argPos);
    if (_result is MySubclassedResult result)
    {
        var builder = new EmbedBuilder();
        for (var pair in result.Data)
        {
            builder.AddField(pair.Key, pair.Value, true);
        }
        await message.Channel.SendMessageAsync("", embed: builder);
    }
    else if (_result is RuntimeResult result)
    {
        await message.Channel.SendMessageAsync(result.Reason);
    }
    else if (!_result.IsSuccess)
    {
        // Previous error handling
    }
    ```

---
+++ src/Discord.Net.Commands/Results/RuntimeResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Discord.Commands
+{
+    [DebuggerDisplay(@"{DebuggerDisplay,nq}")]
+    public class RuntimeResult : IResult
+    {
+        private RuntimeResult(CommandError? error, string reason)
+        {
+            Error = error;
+            Reason = reason;
+        }
+
+        public CommandError? Error { get; }
+        public string Reason { get; }
+
+        public bool IsSuccess => !Error.HasValue;
+
+        string IResult.ErrorReason => Reason;
+
+        public static RuntimeResult FromSuccess(string reason = null) =>
+            new RuntimeResult(null, reason);
+        public static RuntimeResult FromError(string reason) =>
+            new RuntimeResult(CommandError.Unsuccessful, reason);
+        public static RuntimeResult FromError(IResult result) =>
+            new RuntimeResult(result.Error, result.ErrorReason);
+
+        public override string ToString() => Reason ?? (IsSuccess ? "Successful" : "Unsuccessful");
+        private string DebuggerDisplay => IsSuccess ? $"Success: {Reason ?? "No Reason"}" : $"{Error}: {Reason}";
+    }
+}
