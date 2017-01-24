7c5fe79333dd95be1ec2858afe95b94e732fbfe5
RogueException <rogueexception@gmail.com>
2017-01-24 15:40:41 -0400

    Updated build script

---
+++ build.ps1
@@ -0,0 +1,15 @@
+if (Test-Path Env:\APPVEYOR_BUILD_NUMBER) {
+    $build = [convert]::ToInt32($env:APPVEYOR_BUILD_NUMBER).ToString("00000")
+} else {
+    $build = "dev"
+}
+
+dotnet restore Discord.Net.sln
+dotnet pack "src\Discord.Net\Discord.Net.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
+dotnet pack "src\Discord.Net.Core\Discord.Net.Core.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
+dotnet pack "src\Discord.Net.Commands\Discord.Net.Commands.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
+dotnet pack "src\Discord.Net.Rest\Discord.Net.Rest.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
+dotnet pack "src\Discord.Net.WebSocket\Discord.Net.WebSocket.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
+dotnet pack "src\Discord.Net.Rpc\Discord.Net.Rpc.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
+dotnet pack "src\Discord.Net.Providers.WS4Net\Discord.Net.Providers.WS4Net.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
+dotnet pack "src\Discord.Net.Providers.UdpClient\Discord.Net.Providers.UdpClient.csproj" -c "Release" -o "artifacts" -p:BuildNumber="$build"
