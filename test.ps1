3046b62c94b6e091fb970408d3311ec038de488d
RogueException <rogueexception@gmail.com>
2017-01-25 12:41:38 -0400

    Cleaned up csproj/sln/scripts

--- test.ps1
+++ test.ps1
@@ -1,4 +1,2 @@
-dotnet restore test/Discord.Net.Tests/Discord.Net.Tests.csproj
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
-dotnet test test/Discord.Net.Tests/Discord.Net.Tests.csproj
+dotnet test test/Discord.Net.Tests/Discord.Net.Tests.csproj -c "Release" --noBuild -p:BuildNumber="$build"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
