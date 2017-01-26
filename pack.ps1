eb2a946dd6183d32bdb9c6d5e73d6a1f7a449cd5
RogueException <rogueexception@gmail.com>
2017-01-25 22:27:24 -0400

    Updated to 1.0.0-rc3 tooling

--- pack.ps1
+++ pack.ps1
@@ -1,12 +1,3 @@
-#dotnet restore "src\Discord.Net\Discord.Net.csproj" /p:BuildNumber="$Env:BUILD"
-#dotnet restore "src\Discord.Net.Core\Discord.Net.Core.csproj" /p:BuildNumber="$Env:BUILD"
-#dotnet restore "src\Discord.Net.Commands\Discord.Net.Commands.csproj" /p:BuildNumber="$Env:BUILD"
-#dotnet restore "src\Discord.Net.Rest\Discord.Net.Rest.csproj" /p:BuildNumber="$Env:BUILD"
-#dotnet restore "src\Discord.Net.WebSocket\Discord.Net.WebSocket.csproj" /p:BuildNumber="$Env:BUILD"
-#dotnet restore "src\Discord.Net.Rpc\Discord.Net.Rpc.csproj" /p:BuildNumber="$Env:BUILD"
-#dotnet restore "src\Discord.Net.Providers.WS4Net\Discord.Net.Providers.WS4Net.csproj" /p:BuildNumber="$Env:BUILD"
-#dotnet restore "src\Discord.Net.Providers.UdpClient\Discord.Net.Providers.UdpClient.csproj" /p:BuildNumber="$Env:BUILD"
-
 dotnet pack "src\Discord.Net\Discord.Net.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD"
 dotnet pack "src\Discord.Net.Core\Discord.Net.Core.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD"
 dotnet pack "src\Discord.Net.Commands\Discord.Net.Commands.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD"
