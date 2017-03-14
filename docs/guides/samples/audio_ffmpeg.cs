ab43f8277150a6fc231cff57132899b28b5f5f0f
William Haskell <will09600@gmail.com>
2017-03-14 18:47:28 +0100

    Update audio_ffmpeg.cs
    AudioApplication is a required argument

--- docs/guides/samples/audio_ffmpeg.cs
+++ docs/guides/samples/audio_ffmpeg.cs
@@ -3,7 +3,7 @@ private async Task SendAsync(IAudioClient client, string path)
     // Create FFmpeg using the previous example
     var ffmpeg = CreateStream(path);
     var output = ffmpeg.StandardOutput.BaseStream;
-    var discord = client.CreatePCMStream(1920);
+    var discord = client.CreatePCMStream(AudioApplication.Mixed, 1920);
     await output.CopyToAsync(discord);
     await discord.FlushAsync();
 }
