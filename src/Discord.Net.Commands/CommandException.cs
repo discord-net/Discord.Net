bbd45a6f4b5908c514d40bffa1c1fa126909c4db
RogueException <rogueexception@gmail.com>
2017-03-25 11:19:31 -0300

    Fixed CommandException

--- src/Discord.Net.Commands/CommandException.cs
+++ src/Discord.Net.Commands/CommandException.cs
@@ -5,11 +5,13 @@ namespace Discord.Commands
     public class CommandException : Exception
     {
         public CommandInfo Command { get; }
-        public ICommandContext Content { get; }
+        public ICommandContext Context { get; }
 
         public CommandException(CommandInfo command, ICommandContext context, Exception ex)
             : base($"Error occurred executing {command.GetLogText(context)}.", ex)
         {
+            Command = command;
+            Context = context;
         }
     }
 }
