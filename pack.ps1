73dfb8c699e2d968893e7f87852c0217a453978a
RogueException <rogueexception@gmail.com>
2017-04-01 11:23:38 -0300

    Added Discord.Net.Webhook to pack list

--- pack.ps1
+++ pack.ps1
@@ -8,6 +8,8 @@ dotnet pack "src\Discord.Net.Rpc\Discord.Net.Rpc.csproj" -c "Release" -o "../../
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 dotnet pack "src\Discord.Net.Commands\Discord.Net.Commands.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+dotnet pack "src\Discord.Net.Webhook\Discord.Net.Webhook.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 dotnet pack "src\Discord.Net.Providers.WS4Net\Discord.Net.Providers.WS4Net.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 dotnet pack "src\Discord.Net.Providers.UdpClient\Discord.Net.Providers.UdpClient.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
