b4c3427ed1bf349f51409b66415e57a2b97bbe2b
Christopher F <foxbot@protonmail.com>
2017-04-03 16:26:58 -0400

    Deleted FAQ
    this is sloppy and doesn't properly explain anything

--- docs/guides/samples/faq/avatar.cs
+++
@@ -1,5 +0,0 @@
-public async Task ChangeAvatar()
-{
-    var fileStream = new FileStream("./newAvatar.png", FileMode.Open);
-    await _client.CurrentUser.ModifyAsync(x => x.Avatar = fileStream);
-}
--- docs/guides/samples/faq/send_message.cs
+++
@@ -1,6 +0,0 @@
-public async Task SendMessageToChannel(ulong ChannelId)
-{
-    var channel = _client.GetChannel(ChannelId) as SocketMessageChannel;
-    await channel?.SendMessageAsync("aaaaaaaaahhh!!!")
-    /*           ^ This question mark is used to indicate that 'channel' may sometimes be null, and in cases that it is null, we will do nothing here. */ 
-}
