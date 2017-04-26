294ffa37291b87035ea5fb1e54d97788031d46eb
Bond-009 <Bond-009@users.noreply.github.com>
2017-04-26 17:41:32 +0200

    Remove Discord.Net.Providers.UdpClient from pack.ps (#627)
    This should fix the appveyor build.

--- pack.ps1
+++ pack.ps1
@@ -12,8 +12,6 @@ dotnet pack "src\Discord.Net.Webhook\Discord.Net.Webhook.csproj" -c "Release" -o
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 dotnet pack "src\Discord.Net.Providers.WS4Net\Discord.Net.Providers.WS4Net.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
-dotnet pack "src\Discord.Net.Providers.UdpClient\Discord.Net.Providers.UdpClient.csproj" -c "Release" -o "../../artifacts" --no-build /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 
 if ($Env:APPVEYOR_REPO_TAG -eq "true") {
     nuget pack src\Discord.Net\Discord.Net.nuspec -OutputDirectory "artifacts" -properties suffix=""
