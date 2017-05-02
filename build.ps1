5ad2d7361d31458012b320ef85b27149d86daf42
RogueException <rogueexception@gmail.com>
2017-05-02 01:03:04 -0300

    Added appveyor.yml, README edits (#639)

--- build.ps1
+++
@@ -1,4 +0,0 @@
-appveyor-retry dotnet restore Discord.Net.sln -v Minimal /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
-dotnet build Discord.Net.sln -c "Release" /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
--- test.ps1
+++
@@ -1,2 +0,0 @@
-dotnet test test/Discord.Net.Tests/Discord.Net.Tests.csproj -c "Release" --no-build /p:BuildNumber="$Env:BUILD" /p:IsTagBuild="$Env:APPVEYOR_REPO_TAG"
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
