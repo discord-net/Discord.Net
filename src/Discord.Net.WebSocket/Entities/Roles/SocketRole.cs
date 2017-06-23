707ec9571786db888dc01e804a30265b6f355931
Alex Gravely <tcbskater@hotmail.com>
2017-06-23 11:01:44 -0400

    Add SocketRole.Members property (#659)
    * Add SocketRole.Members property
    
    * Change Members to IEnumerable.

--- src/Discord.Net.WebSocket/Entities/Roles/SocketRole.cs
+++ src/Discord.Net.WebSocket/Entities/Roles/SocketRole.cs
@@ -1,6 +1,8 @@
 ﻿using Discord.Rest;
 using System;
+using System.Collections.Generic;  
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Model = Discord.API.Role;
 
@@ -22,6 +24,8 @@ namespace Discord.WebSocket
         public DateTimeOffset CreatedAt => SnowflakeUtils.FromSnowflake(Id);
         public bool IsEveryone => Id == Guild.Id;
         public string Mention => MentionUtils.MentionRole(Id);
+        public IEnumerable<SocketGuildUser> Members 
+            => Guild.Users.Where(x => x.Roles.Any(r => r.Id == Id));
 
         internal SocketRole(SocketGuild guild, ulong id)
             : base(guild.Discord, id)
