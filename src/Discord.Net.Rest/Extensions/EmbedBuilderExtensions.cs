3b78817c544d5a35fb0c0e7b9f395b9c353da6f6
RogueException <rogueexception@gmail.com>
2017-06-29 19:45:02 -0300

    Added int overload to EmbedBuilderExtensions

--- src/Discord.Net.Rest/Extensions/EmbedBuilderExtensions.cs
+++ src/Discord.Net.Rest/Extensions/EmbedBuilderExtensions.cs
@@ -8,6 +8,9 @@ namespace Discord
         public static EmbedBuilder WithColor(this EmbedBuilder builder, byte r, byte g, byte b) =>
             builder.WithColor(new Color(r, g, b));
 
+        public static EmbedBuilder WithColor(this EmbedBuilder builder, int r, int g, int b) =>
+            builder.WithColor(new Color(r, g, b));
+
         public static EmbedBuilder WithColor(this EmbedBuilder builder, float r, float g, float b) =>
             builder.WithColor(new Color(r, g, b));
 
