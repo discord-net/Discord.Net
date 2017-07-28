5848ae0d08cc457de903d6358ddfbff92b76b8e6
AntiTcb <tcbskater@hotmail.com>
2017-07-28 00:55:26 -0400

    I'm good at this, I swear. >_>

--- src/Discord.Net.Rpc/DiscordRpcClient.cs
+++ src/Discord.Net.Rpc/DiscordRpcClient.cs
@@ -22,7 +22,7 @@ namespace Discord.Rpc
 
         public ConnectionState ConnectionState { get; private set; }
         public IReadOnlyCollection<string> Scopes { get; private set; }
-        public DateTimeOffset TokenExpiresAt { get; private set; }                                                                                  
+        public DateTimeOffset TokenExpiresAt { get; private set; }
 
         internal new API.DiscordRpcApiClient ApiClient => base.ApiClient as API.DiscordRpcApiClient;
         public new RestSelfUser CurrentUser { get { return base.CurrentUser as RestSelfUser; } private set { base.CurrentUser = value; } }
@@ -66,7 +66,7 @@ namespace Discord.Rpc
         }
 
         public Task StartAsync() => _connection.StartAsync();
-        public Task StopAsync() => _connection.StopAsync();                                                                                                                             
+        public Task StopAsync() => _connection.StopAsync();
 
         private async Task OnConnectingAsync()
         {
