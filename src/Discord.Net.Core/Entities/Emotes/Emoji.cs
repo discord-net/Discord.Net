40ba2983319eada5357176bd2a2a8af45cbeb052
Christopher F <computerizedtaco@gmail.com>
2017-08-13 16:47:07 -0400

    Added emoji validation

--- src/Discord.Net.Core/Entities/Emotes/Emoji.cs
+++ src/Discord.Net.Core/Entities/Emotes/Emoji.cs
@@ -1,9 +1,12 @@
-﻿namespace Discord
+﻿using System;
+using System.Text;
+
+namespace Discord
 {
     /// <summary>
     /// A unicode emoji
     /// </summary>
-    public class Emoji : IEmote
+    public partial class Emoji : IEmote
     {
         // TODO: need to constrain this to unicode-only emojis somehow
 
@@ -20,6 +23,31 @@
         /// <param name="unicode">The pure UTF-8 encoding of an emoji</param>
         public Emoji(string unicode)
         {
+            byte[] utf32 = Encoding.Unicode.GetBytes(unicode);
+            bool safe = false, any = false;
+            int codepoint = 0;
+            for (var i = 0; i < utf32.Length; i += 4)
+            {
+                codepoint = BitConverter.ToInt32(utf32, i);
+                any = false;
+                for (var j = 0; j < Codepoints.Length; j++)
+                {
+                    if (Codepoints[j] == codepoint)
+                    {
+                        any = true;
+                        break;
+                    }
+                }
+                if (any)
+                    safe = true;
+                else
+                {
+                    safe = false;
+                    break;
+                }
+            }
+            if (!safe)
+                throw new ArgumentException("One or more characters was not a valid Emoji", nameof(unicode));
             Name = unicode;
         }
 
---
+++ test/Discord.Net.Tests/Tests.Emotes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace Discord
+{
+    public class EmoteTests
+    {
+		[Fact]
+		public void Emoji()
+        {
+			// Future: Validate emoji parsing
+            Assert.Equal("🦅", new Emoji("🦅").Name);
+        }
+
+		[Fact]
+		public void Emote()
+        {
+            Assert.Equal(true, Discord.Emote.TryParse("<:foxDab:280494667093508096>", out var emote));
+            Assert.NotNull(emote);
+            Assert.Equal("foxDab", emote.Name);
+            Assert.Equal(280494667093508096UL, emote.Id);
+            Assert.Equal(DateTimeOffset.FromUnixTimeMilliseconds(1486945539974), emote.CreatedAt);
+        }
+		[Fact]
+		public void Emote_Parse_Fail()
+        {
+            Assert.Equal(false, Discord.Emote.TryParse("", out _));
+            Assert.Equal(false, Discord.Emote.TryParse(":foxDab", out _));
+            Assert.Equal(false, Discord.Emote.TryParse(":foxDab:", out _));
+            Assert.Throws<ArgumentException>(() => Discord.Emote.Parse(":foxDab:"));
+        }
+    }
+}
