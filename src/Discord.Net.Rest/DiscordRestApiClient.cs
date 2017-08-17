3b6133fd4708acd5cf78051638c3d58ef4d86ca0
RogueException <rogueexception@gmail.com>
2017-08-17 02:58:07 -0300

    Removed braces

--- src/Discord.Net.Rest/DiscordRestApiClient.cs
+++ src/Discord.Net.Rest/DiscordRestApiClient.cs
@@ -1003,9 +1003,7 @@ namespace Discord.API
             bool isCurrentUser = userId == CurrentUserId;
 
             if (args.RoleIds.IsSpecified)
-            {
                 Preconditions.NotEveryoneRole(args.RoleIds.Value, guildId, nameof(args.RoleIds));
-            }
             if (isCurrentUser && args.Nickname.IsSpecified)
             {
                 var nickArgs = new Rest.ModifyCurrentUserNickParams(args.Nickname.Value ?? "");
