60df7287141fb50ff8524cd38307c134728dd00e
Alex Gravely <tcbskater@hotmail.com>
2017-09-26 01:03:52 -0400

    Removing the UserPresenceUpdated event.

--- src/Discord.Net.WebSocket/DiscordShardedClient.Events.cs
+++ src/Discord.Net.WebSocket/DiscordShardedClient.Events.cs
@@ -34,13 +34,5 @@ namespace Discord.WebSocket
             remove { _shardLatencyUpdatedEvent.Remove(value); }
         }
         private readonly AsyncEvent<Func<int, int, DiscordSocketClient, Task>> _shardLatencyUpdatedEvent = new AsyncEvent<Func<int, int, DiscordSocketClient, Task>>();
-
-        [Obsolete("This event is non-functioning and will be removed in the next major release. Functionality has been merged into the GuildMemberUpdated event.")]
-        public event Func<Optional<SocketGuild>, SocketUser, SocketPresence, SocketPresence, Task> UserPresenceUpdated
-        {
-            add { _userPresenceUpdatedEvent.Add(value); }
-            remove { _userPresenceUpdatedEvent.Remove(value); }
-        }
-        private readonly AsyncEvent<Func<Optional<SocketGuild>, SocketUser, SocketPresence, SocketPresence, Task>> _userPresenceUpdatedEvent = new AsyncEvent<Func<Optional<SocketGuild>, SocketUser, SocketPresence, SocketPresence, Task>>();
     }
 }
