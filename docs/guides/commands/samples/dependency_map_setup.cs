7774602e928b40968a28fb8a1fce978b8ad7d8b8
Hsu Still <341464@gmail.com>
2017-09-30 12:12:00 +0800

    Fix DepMap sample formatting

--- docs/guides/commands/samples/dependency_map_setup.cs
+++ docs/guides/commands/samples/dependency_map_setup.cs
@@ -6,13 +6,13 @@ public async Task InstallAsync(DiscordSocketClient client)
 	// Here, we will inject the ServiceProvider with 
 	// all of the services our client will use.
 	_services = new ServiceCollection()
-					.AddSingleton(client)
-					.AddSingleton(_commands)
-					// You can pass in an instance of the desired type
-					.AddSingleton(new NotificationService())
-					// ...or by using the generic method.
-					.AddSingleton<DatabaseService>()
-					.BuildServiceProvider();
+		.AddSingleton(client)
+		.AddSingleton(_commands)
+		// You can pass in an instance of the desired type
+		.AddSingleton(new NotificationService())
+		// ...or by using the generic method.
+		.AddSingleton<DatabaseService>()
+		.BuildServiceProvider();
 	// ...
 	await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
 }
