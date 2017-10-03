774cf4da55693493c9fc9573f552efb3e102f348
Vollrat <29968697+Vollrat@users.noreply.github.com>
2017-10-03 21:19:54 -0230

    "DefaultRunMode should also be "Gets or sets blah blah blah" to be consistent."

--- src/Discord.Net.Commands/CommandServiceConfig.cs
+++ src/Discord.Net.Commands/CommandServiceConfig.cs
@@ -2,7 +2,7 @@
 {
     public class CommandServiceConfig
     {
-        /// <summary> The default RunMode commands should have, if one is not specified on the Command attribute or builder. </summary>
+        /// <summary> Gets or sets the default RunMode commands should have, if one is not specified on the Command attribute or builder. </summary>
         public RunMode DefaultRunMode { get; set; } = RunMode.Sync;
 
         public char SeparatorChar { get; set; } = ' ';
