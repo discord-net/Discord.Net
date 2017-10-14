8b537f58c056001d1598fdb379c6b7ee96cd60c2
Christopher F <foxbot@protonmail.com>
2017-10-14 15:17:27 -0400

    Added MicrosoftLogging project + LogAdaptor
    The Log event needed to be added to IDiscordClient to allow
    MicrosoftLogging to compile implementation-agnostic. This has no major
    effect on any of our libraries, and should have minimal effect on any
    downstream libraries.

---
+++ src/Discord.Net.MicrosoftLogging/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Discord.MicrosoftLogging
+{
+    public static class Extensions
+    {
+        public static void UseMicrosoftLogging(this IDiscordClient client, ILogger logger, Func<LogMessage, Exception, string> formatter = null)
+        {
+            var adaptor = new LogAdaptor(logger, formatter);
+            client.Log += adaptor.Log;
+        }
+    }
+}
