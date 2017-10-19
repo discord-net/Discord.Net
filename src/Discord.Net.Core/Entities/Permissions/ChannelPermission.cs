6aab231d2aeee9e8d9c4bc9636ca2892e5d441ef
Chris Johnston <Chris-Johnston@users.noreply.github.com>
2017-10-18 22:16:15 -0700

    Cleaned up the formatting of ChannelPermission and GuildPermission enums to make it easier to read

--- src/Discord.Net.Core/Entities/Permissions/ChannelPermission.cs
+++ src/Discord.Net.Core/Entities/Permissions/ChannelPermission.cs
@@ -7,30 +7,30 @@ namespace Discord
     {
         // General
         CreateInstantInvite = 0x00_00_00_01,
-        ManageChannels = 0x00_00_00_10,
+        ManageChannels      = 0x00_00_00_10,
 
         // Text
-        AddReactions = 0x00_00_00_40,
-        ReadMessages = 0x00_000400,
-        SendMessages = 0x00_00_08_00,
-        SendTTSMessages = 0x00_00_10_00,
-        ManageMessages = 0x00_00_20_00,
-        EmbedLinks = 0x00_00_40_00,
-        AttachFiles = 0x00_00_80_00,
-        ReadMessageHistory = 0x00_01_00_00,
-        MentionEveryone = 0x00_02_00_00,
-        UseExternalEmojis = 0x00_04_00_00,
+        AddReactions        = 0x00_00_00_40,
+        ReadMessages        = 0x00_00_04_00,
+        SendMessages        = 0x00_00_08_00,
+        SendTTSMessages     = 0x00_00_10_00,
+        ManageMessages      = 0x00_00_20_00,
+        EmbedLinks          = 0x00_00_40_00,
+        AttachFiles         = 0x00_00_80_00,
+        ReadMessageHistory  = 0x00_01_00_00,
+        MentionEveryone     = 0x00_02_00_00,
+        UseExternalEmojis   = 0x00_04_00_00,
 
         // Voice
-        Connect = 0x00_10_00_00,
-        Speak = 0x00_20_00_00,
-        MuteMembers = 0x00_40_00_00,
-        DeafenMembers = 0x00_80_00_00,
-        MoveMembers = 0x01_00_00_00,
-        UseVAD = 0x02_00_00_00,
+        Connect             = 0x00_10_00_00,
+        Speak               = 0x00_20_00_00,
+        MuteMembers         = 0x00_40_00_00,
+        DeafenMembers       = 0x00_80_00_00,
+        MoveMembers         = 0x01_00_00_00,
+        UseVAD              = 0x02_00_00_00,
 
         // More General
-        ManageRoles = 0x10_00_00_00,
-        ManageWebhooks = 0x20_00_00_00,
+        ManageRoles         = 0x10_00_00_00,
+        ManageWebhooks      = 0x20_00_00_00,
     }
 }
--- src/Discord.Net.Core/Entities/Permissions/GuildPermission.cs
+++ src/Discord.Net.Core/Entities/Permissions/GuildPermission.cs
@@ -7,38 +7,38 @@ namespace Discord
     {
         // General
         CreateInstantInvite = 0x00_00_00_01,
-        KickMembers = 0x00_00_00_02,
-        BanMembers = 0x00_00_00_04,
-        Administrator = 0x00_00_00_08,
-        ManageChannels = 0x00_00_00_10,
-        ManageGuild = 0x00_00_00_20,
+        KickMembers         = 0x00_00_00_02,
+        BanMembers          = 0x00_00_00_04,
+        Administrator       = 0x00_00_00_08,
+        ManageChannels      = 0x00_00_00_10,
+        ManageGuild         = 0x00_00_00_20,
 
         // Text
-        AddReactions = 0x00_00_00_40,
-        ViewAuditLog = 0x00_00_00_80,
-        ReadMessages = 0x00_00_04_00,
-        SendMessages = 0x00_00_08_00,
-        SendTTSMessages = 0x00_00_10_00,
-        ManageMessages = 0x00_00_20_00,
-        EmbedLinks = 0x00_00_40_00,
-        AttachFiles = 0x00_00_80_00,
-        ReadMessageHistory = 0x00_01_00_00,
-        MentionEveryone = 0x00_02_00_00,
-        UseExternalEmojis = 0x00_04_00_00,
+        AddReactions        = 0x00_00_00_40,
+        ViewAuditLog        = 0x00_00_00_80,
+        ReadMessages        = 0x00_00_04_00,
+        SendMessages        = 0x00_00_08_00,
+        SendTTSMessages     = 0x00_00_10_00,
+        ManageMessages      = 0x00_00_20_00,
+        EmbedLinks          = 0x00_00_40_00,
+        AttachFiles         = 0x00_00_80_00,
+        ReadMessageHistory  = 0x00_01_00_00,
+        MentionEveryone     = 0x00_02_00_00,
+        UseExternalEmojis   = 0x00_04_00_00,
 
         // Voice
-        Connect = 0x00_10_00_00,
-        Speak = 0x00_20_00_00,
-        MuteMembers = 0x00_40_00_00,
-        DeafenMembers = 0x00_80_00_00,
-        MoveMembers = 0x01_00_00_00,
-        UseVAD = 0x02_00_00_00,
+        Connect             = 0x00_10_00_00,
+        Speak               = 0x00_20_00_00,
+        MuteMembers         = 0x00_40_00_00,
+        DeafenMembers       = 0x00_80_00_00,
+        MoveMembers         = 0x01_00_00_00,
+        UseVAD              = 0x02_00_00_00,
 
         // General 2
-        ChangeNickname = 0x04_00_00_00,
-        ManageNicknames = 0x08_00_00_00,
-        ManageRoles = 0x10_00_00_00,
-        ManageWebhooks = 0x20_00_00_00,
-        ManageEmojis = 0x40_00_00_00
+        ChangeNickname      = 0x04_00_00_00,
+        ManageNicknames     = 0x08_00_00_00,
+        ManageRoles         = 0x10_00_00_00,
+        ManageWebhooks      = 0x20_00_00_00,
+        ManageEmojis        = 0x40_00_00_00
     }
 }
