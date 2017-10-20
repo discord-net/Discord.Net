dbaf84ddfbc905e125e4370fb2847071b79d84ff
FiniteReality <digpoealternate@gmail.com>
2017-10-20 09:44:11 +0100

    Remove payload compression, use stream compression
    This should help improve #850 in theory.

--- src/Discord.Net.WebSocket/API/Gateway/IdentifyParams.cs
+++ src/Discord.Net.WebSocket/API/Gateway/IdentifyParams.cs
@@ -13,8 +13,6 @@ namespace Discord.API.Gateway
         public IDictionary<string, string> Properties { get; set; }
         [JsonProperty("large_threshold")]
         public int LargeThreshold { get; set; }
-        [JsonProperty("compress")]
-        public bool UseCompression { get; set; }
         [JsonProperty("shard")]
         public Optional<int[]> ShardingParams { get; set; }
     }
