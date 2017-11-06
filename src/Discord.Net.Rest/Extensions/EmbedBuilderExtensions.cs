5218e6be97aa20428104366afb34bf6e0800c451
Alex Gravely <tcbskater@hotmail.com>
2017-11-05 21:57:24 -0500

    Add IEmbed#ToEmbedBuilder extension method (#863)
    * Add IEmbed#ToEmbedBuilder extension method
    
    * Implementing reviewed changes.
    
    * Switch to object initializers for author and footer.

--- src/Discord.Net.Rest/Extensions/EmbedBuilderExtensions.cs
+++ src/Discord.Net.Rest/Extensions/EmbedBuilderExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Discord
 {
     public static class EmbedBuilderExtensions
@@ -19,5 +21,38 @@ namespace Discord
 
         public static EmbedBuilder WithAuthor(this EmbedBuilder builder, IGuildUser user) =>
             builder.WithAuthor($"{user.Nickname ?? user.Username}#{user.Discriminator}", user.GetAvatarUrl());
+
+        public static EmbedBuilder ToEmbedBuilder(this IEmbed embed)
+        {
+            if (embed.Type != EmbedType.Rich)
+                throw new InvalidOperationException($"Only {nameof(EmbedType.Rich)} embeds may be built.");
+
+            var builder = new EmbedBuilder
+            {
+                Author = new EmbedAuthorBuilder
+                {
+                    Name = embed.Author?.Name,
+                    IconUrl = embed.Author?.IconUrl,
+                    Url = embed.Author?.Url
+                },
+                Color = embed.Color ?? Color.Default,
+                Description = embed.Description,
+                Footer = new EmbedFooterBuilder
+                {
+                    Text = embed.Footer?.Text,
+                    IconUrl = embed.Footer?.IconUrl
+                },
+                ImageUrl = embed.Image?.Url,
+                ThumbnailUrl = embed.Thumbnail?.Url,
+                Timestamp = embed.Timestamp,
+                Title = embed.Title,
+                Url = embed.Url
+            };
+
+            foreach (var field in embed.Fields)
+                builder.AddField(field.Name, field.Value, field.Inline);
+
+            return builder;
+        }
     }
 }
