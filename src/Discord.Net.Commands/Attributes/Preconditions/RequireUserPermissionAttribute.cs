e5dfb6c3e54f09e51223b5706ce96ddec97c8291
Christopher F <foxbot@protonmail.com>
2017-11-21 16:33:46 -0500

    Fix null channel being passed in RequirePermission preconditions (#886)
    * Fix null channel being passed in RequirePermission preconditions
    
    * c#7 pattern matching

--- src/Discord.Net.Commands/Attributes/Preconditions/RequireUserPermissionAttribute.cs
+++ src/Discord.Net.Commands/Attributes/Preconditions/RequireUserPermissionAttribute.cs
@@ -56,13 +56,11 @@ namespace Discord.Commands
 
             if (ChannelPermission.HasValue)
             {
-                var guildChannel = context.Channel as IGuildChannel;
-
                 ChannelPermissions perms;
-                if (guildChannel != null)
+                if (context.Channel is IGuildChannel guildChannel)
                     perms = guildUser.GetPermissions(guildChannel);
                 else
-                    perms = ChannelPermissions.All(guildChannel);
+                    perms = ChannelPermissions.All(context.Channel);
 
                 if (!perms.Has(ChannelPermission.Value))
                     return Task.FromResult(PreconditionResult.FromError($"User requires channel permission {ChannelPermission.Value}"));
