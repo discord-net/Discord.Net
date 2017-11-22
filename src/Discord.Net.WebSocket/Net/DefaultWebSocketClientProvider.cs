8f53086499b2dd62817d582b081f9ab1a095ad4b
Christopher F <foxbot@protonmail.com>
2017-11-22 18:19:04 -0500

    Allow opting in to proxies on the WebSocket

--- src/Discord.Net.WebSocket/Net/DefaultWebSocketClientProvider.cs
+++ src/Discord.Net.WebSocket/Net/DefaultWebSocketClientProvider.cs
@@ -1,21 +1,27 @@
 using System;
+using System.Net;
 
 namespace Discord.Net.WebSockets
 {
     public static class DefaultWebSocketProvider
     {
 #if DEFAULTWEBSOCKET
-        public static readonly WebSocketProvider Instance = () => 
+        public static readonly WebSocketProvider Instance = Create();
+
+        public static WebSocketProvider Create(IWebProxy proxy = null)
         {
-            try
+            return () =>
             {
-                return new DefaultWebSocketClient();                    
-            }
-            catch (PlatformNotSupportedException ex)
-            {
-                throw new PlatformNotSupportedException("The default WebSocketProvider is not supported on this platform.", ex);
-            }
-        };
+                try
+                {
+                    return new DefaultWebSocketClient(proxy);
+                }
+                catch (PlatformNotSupportedException ex)
+                {
+                    throw new PlatformNotSupportedException("The default WebSocketProvider is not supported on this platform.", ex);
+                }
+            };
+        }
 #else
         public static readonly WebSocketProvider Instance = () =>
         {
