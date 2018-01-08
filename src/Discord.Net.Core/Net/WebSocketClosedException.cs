2041e475ddfaab26e9c7d26ed56dede51c5a0209
Christopher F <computerizedtaco@gmail.com>
2018-01-07 23:43:31 -0500

    Remove unused 'fatal' bool in WebSocketClosedException#ctor

--- src/Discord.Net.Core/Net/WebSocketClosedException.cs
+++ src/Discord.Net.Core/Net/WebSocketClosedException.cs
@@ -7,7 +7,7 @@ namespace Discord.Net
         public int CloseCode { get; }
         public string Reason { get; }
 
-        public WebSocketClosedException(int closeCode, string reason = null, bool fatal = false)
+        public WebSocketClosedException(int closeCode, string reason = null)
             : base($"The server sent close {closeCode}{(reason != null ? $": \"{reason}\"" : "")}")
         {
             CloseCode = closeCode;
