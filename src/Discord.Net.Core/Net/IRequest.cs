cb5427a7d02d072196056f932ed5e5818d678a73
Alex Gravely <tcbskater@hotmail.com>
2018-02-17 18:40:18 -0500

    Remove Promise from interface.

--- src/Discord.Net.Core/Net/IRequest.cs
+++ src/Discord.Net.Core/Net/IRequest.cs
@@ -1,14 +1,10 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace Discord.Net
 {
     public interface IRequest
     {
         DateTimeOffset? TimeoutAt { get; }
-        TaskCompletionSource<Stream> Promise { get; }
         RequestOptions Options { get; }
-        
     }
 }
