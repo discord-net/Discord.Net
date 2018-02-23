d5a503f1cb21345b2549dc5002026093178d3882
Darnell Williams <williams.darnell56@yahoo.com>
2018-02-22 22:26:29 -0500

    Expose method from interface

--- src/Discord.Net.Core/IDiscordClient.cs
+++ src/Discord.Net.Core/IDiscordClient.cs
@@ -36,5 +36,7 @@ namespace Discord
         Task<IVoiceRegion> GetVoiceRegionAsync(string id, RequestOptions options = null);
 
         Task<IWebhook> GetWebhookAsync(ulong id, RequestOptions options = null);
+
+        Task<int> GetRecommendedShardCount(RequestOptions options = null);
     }
 }
