b9be6deb4f07adca13d33f23cfc9c8d21f0297e9
Christopher F <foxbot@protonmail.com>
2018-03-18 15:59:45 -0400

    Move EmbedBuilder+Extensions to Discord.Net.Core
    Previously it was implemented under Discord.Net.Rest, which seems
    inconsistent and unnecessary.
    
    This also allows Commands docstrings to reference EmbedBuilder, since
    Commands only has a dependency on Core.

--- src/Discord.Net.Rest/AssemblyInfo.cs
+++ src/Discord.Net.Rest/AssemblyInfo.cs
@@ -1,7 +1,10 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Discord.Net.Rpc")]
 [assembly: InternalsVisibleTo("Discord.Net.WebSocket")]
 [assembly: InternalsVisibleTo("Discord.Net.Webhook")]
 [assembly: InternalsVisibleTo("Discord.Net.Commands")]
 [assembly: InternalsVisibleTo("Discord.Net.Tests")]
+
+[assembly: TypeForwardedTo(typeof(Discord.EmbedBuilder))]
+[assembly: TypeForwardedTo(typeof(Discord.EmbedBuilderExtensions))]
