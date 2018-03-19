e6b00979f9d8dbafb119140049778e7ad8e767f1
Hsu Still <341464@gmail.com>
2018-03-19 15:14:55 +0800

    Add RunMode documentation

--- src/Discord.Net.Commands/RunMode.cs
+++ src/Discord.Net.Commands/RunMode.cs
@@ -1,9 +1,18 @@
-﻿namespace Discord.Commands
+namespace Discord.Commands
 {
     public enum RunMode
     {
+        /// <summary>
+        /// Default <see cref="RunMode"/> behaviour set in <see cref="CommandServiceConfig"/>.
+        /// </summary>
         Default,
+        /// <summary>
+        /// Executes the command on the same thread as gateway.
+        /// </summary>
         Sync,
+        /// <summary>
+        /// Executes the command on a different thread from the gateway one.
+        /// </summary>
         Async
     }
 }
