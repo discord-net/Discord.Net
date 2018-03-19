c6d4494047756a5f2c08b25836200a69dfcb7106
Hsu Still <341464@gmail.com>
2018-03-19 15:21:31 +0800

    Add CommandError xmldocs

--- src/Discord.Net.Commands/CommandError.cs
+++ src/Discord.Net.Commands/CommandError.cs
@@ -1,26 +1,50 @@
-﻿namespace Discord.Commands
+namespace Discord.Commands
 {
     public enum CommandError
     {
         //Search
+        /// <summary>
+        /// Thrown when the command is unknown.
+        /// </summary>
         UnknownCommand = 1,
 
         //Parse
+        /// <summary>
+        /// Thrown when the command fails to be parsed.
+        /// </summary>
         ParseFailed,
+        /// <summary>
+        /// Thrown when the input text has too few parameters.
+        /// </summary>
         BadArgCount,
 
         //Parse (Type Reader)
         //CastFailed,
+        /// <summary>
+        /// Thrown when the object cannot be found by the <see cref="TypeReader"/>.
+        /// </summary>
         ObjectNotFound,
+        /// <summary>
+        /// Thrown when more than one objects is matched by <see cref="TypeReader"/>.
+        /// </summary>
         MultipleMatches,
 
         //Preconditions
+        /// <summary>
+        /// Thrown when the command fails to meet a <see cref="PreconditionAttribute"/>'s conditions.
+        /// </summary>
         UnmetPrecondition,
 
         //Execute
+        /// <summary>
+        /// Thrown when an exception occurs mid-command execution.
+        /// </summary>
         Exception,
 
         //Runtime
+        /// <summary>
+        /// Thrown when the command is not successfully executed on runtime.
+        /// </summary>
         Unsuccessful
     }
 }
