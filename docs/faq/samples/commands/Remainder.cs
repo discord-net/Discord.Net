1d6220187d237d6e76be3bbfa18ee7d76b3317e5
Hsu Still <341464@gmail.com>
2018-03-20 17:27:47 +0800

    Fix broken indent

--- docs/faq/samples/commands/Remainder.cs
+++ docs/faq/samples/commands/Remainder.cs
@@ -12,7 +12,7 @@ public Task EchoRemainderAsync([Remainder]string text) => ReplyAsync(text);
 public Task EchoAsync(string text) => ReplyAsync(text);
 
 //	The message would be seen as having multiple parameters, 
-//  while the method only accepts one. 
+//	while the method only accepts one. 
 //	Wrapping the message in quotes solves this.
 //	This way, the system knows the entire message is to be parsed as a 
 //	single String.
