c572a98dc73dcfc9697b5668f7d90f743c765981
Chris Johnston <githubchrisjohnston@gmail.com>
2018-07-30 22:06:10 -0700

    Added discord API documentation to IConnection interface

--- src/Discord.Net.Core/Entities/Users/IConnection.cs
+++ src/Discord.Net.Core/Entities/Users/IConnection.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Discord
 {
     public interface IConnection
     {
+        /// <summary>
+        ///     ID of the connection account.
+        /// </summary>
         string Id { get; }
+        /// <summary>
+        ///     The service of the connection (twich, youtube).
+        /// </summary>
         string Type { get; }
+        /// <summary>
+        ///     The username of the connection account.
+        /// </summary>
         string Name { get; }
+        /// <summary>
+        ///     Wheter the connection is revoked.
+        /// </summary>
         bool IsRevoked { get; }
 
+        /// <summary>
+        ///     A <see cref="IReadOnlyCollection{T}"/> of integration IDs.
+        /// </summary>
         IReadOnlyCollection<ulong> IntegrationIds { get; }
     }
 }
