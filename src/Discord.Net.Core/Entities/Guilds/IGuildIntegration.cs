c8bc452c68a7fc94e594b7be5b168e0549f96cfd
Chris Johnston <githubchrisjohnston@gmail.com>
2018-07-31 22:52:27 -0700

    Rewrote the returns tags in IGuildIntegration, removed the ones I was unsure about.

--- src/Discord.Net.Core/Entities/Guilds/IGuildIntegration.cs
+++ src/Discord.Net.Core/Entities/Guilds/IGuildIntegration.cs
@@ -5,35 +5,30 @@ namespace Discord
     public interface IGuildIntegration
     {
         /// <summary> Gets the integration ID. </summary>
-        /// <returns> Gets the integration ID. </returns>
+        /// <returns> A unique identifier value of this integration. </returns>
         ulong Id { get; }
         /// <summary> Gets the integration name. </summary>
-        /// <returns> Gets the integration name. </returns>
+        /// <returns> A string containing the name of this integration. </returns>
         string Name { get; }
         /// <summary> Gets the integration type (twitch, youtube, etc). </summary>
-        /// <returns> Gets the integration type (twitch, youtube, etc). </returns>
+        /// <returns> A string containing the name of the type of integration. </returns>
         string Type { get; }
         /// <summary> Gets if this integration is enabled or not. </summary>
-        /// <summary> Gets if this integration is enabled or not. </returns>
+        /// <summary> A value indicating if this integration is enabled. </returns>
         bool IsEnabled { get; }
         /// <summary> Gets if this integration is syncing or not. </summary>
-        /// <returns> Gets if this integration is syncing or not. </returns>
+        /// <returns> A value indicating if this integration is syncing. </returns>
         bool IsSyncing { get; }
         /// <summary> Gets the ID that this integration uses for "subscribers". </summary>
-        /// <returns> Gets the ID that this integration uses for "subscribers". </returns>
         ulong ExpireBehavior { get; }
         /// <summary> Gets the grace period before expiring subscribers. </summary>
-        /// <returns> Gets the grace period before expiring subscribers. </returns>
         ulong ExpireGracePeriod { get; }
         /// <summary> Gets when this integration was last synced. </summary>
-        /// <returns> Gets when this integration was last synced. </returns>
+        /// <returns> A <see cref="DateTimeOffset"/> containing a date and time of day when the integration was last synced. </returns>
         DateTimeOffset SyncedAt { get; }
         /// <summary>
         ///     Gets integration account information. See <see cref="IntegrationAccount"/>.
         /// </summary>
-        /// <returns>
-        ///     Gets integration account information. See <see cref="IntegrationAccount"/>.
-        /// </returns>
         IntegrationAccount Account { get; }
 
         IGuild Guild { get; }
