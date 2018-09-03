60a60bce921454310ebe935ed0c8c95c51381df0
Adomix <33987862+Adomix@users.noreply.github.com>
2018-09-03 15:34:48 -0400

    Update RequestOptions.cs

--- src/Discord.Net.Core/RequestOptions.cs
+++ src/Discord.Net.Core/RequestOptions.cs
@@ -22,6 +22,7 @@ namespace Discord
         internal bool IgnoreState { get; set; }
         internal string BucketId { get; set; }
         internal bool IsClientBucket { get; set; }
+        internal bool IsReactionBucket { get; set; }
 
         internal static RequestOptions CreateOrClone(RequestOptions options)
         {            
