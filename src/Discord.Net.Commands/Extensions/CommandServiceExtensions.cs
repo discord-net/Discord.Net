0133e9889b9aee6d324e5c9d4eecfb2d216ed866
Alex Gravely <tcbskater@hotmail.com>
2018-09-28 19:01:04 -0400

    Resolve build errors.

--- src/Discord.Net.Commands/Extensions/CommandServiceExtensions.cs
+++ src/Discord.Net.Commands/Extensions/CommandServiceExtensions.cs
@@ -24,12 +24,12 @@ namespace Discord.Commands
             return executableCommands;
         }
         public static Task<IReadOnlyCollection<CommandInfo>> GetExecutableCommandsAsync(this CommandService commandService, ICommandContext context, IServiceProvider provider)
-            => GetExecutableCommandsAsync(commandService.Commands, context, provider);
+            => GetExecutableCommandsAsync(commandService.Commands.ToArray(), context, provider);
         public static async Task<IReadOnlyCollection<CommandInfo>> GetExecutableCommandsAsync(this ModuleInfo module, ICommandContext context, IServiceProvider provider)
         {
             var executableCommands = new List<CommandInfo>();
 
-            executableCommands.AddRange(await module.Commands.GetExecutableCommandsAsync(context, provider).ConfigureAwait(false));
+            executableCommands.AddRange(await module.Commands.ToArray().GetExecutableCommandsAsync(context, provider).ConfigureAwait(false));
 
             var tasks = module.Submodules.Select(async s => await s.GetExecutableCommandsAsync(context, provider).ConfigureAwait(false));
             var results = await Task.WhenAll(tasks);
