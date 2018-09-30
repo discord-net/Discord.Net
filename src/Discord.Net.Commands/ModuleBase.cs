661d63836f2adf82b971885410347390f177397c
JustNrik <35231903+JustNrik@users.noreply.github.com>
2018-09-30 16:40:32 -0400

    small code clean up

--- src/Discord.Net.Commands/ModuleBase.cs
+++ src/Discord.Net.Commands/ModuleBase.cs
@@ -18,21 +18,11 @@ namespace Discord.Commands
         /// <param name="isTTS">Specifies if Discord should read this message aloud using TTS</param>
         /// <param name="embed">An embed to be displayed alongside the message</param>
         protected virtual async Task<IUserMessage> ReplyAsync(string message = null, bool isTTS = false, Embed embed = null, RequestOptions options = null)
-        {
-            return await Context.Channel.SendMessageAsync(message, isTTS, embed, options).ConfigureAwait(false);
-        }
+            => await Context.Channel.SendMessageAsync(message, isTTS, embed, options).ConfigureAwait(false);
 
-        protected virtual void BeforeExecute(CommandInfo command)
-        {
-        }
-
-        protected virtual void AfterExecute(CommandInfo command)
-        {
-        }
-
-        protected virtual void OnModuleBuilding(CommandService commandService, ModuleBuilder builder)
-        {
-        }
+        protected virtual void BeforeExecute(CommandInfo command) { }
+        protected virtual void AfterExecute(CommandInfo command) { }
+        protected virtual void OnModuleBuilding(CommandService commandService, ModuleBuilder builder) { }
 
         //IModuleBase
         void IModuleBase.SetContext(ICommandContext context)
