9cbd722b27a822f4c81a4b8674733a54c07a54b8
Still Hsu <341464@gmail.com>
2018-10-08 21:27:53 +0800

    Add warning for Direction.Around

--- src/Discord.Net.Core/Entities/Channels/Direction.cs
+++ src/Discord.Net.Core/Entities/Channels/Direction.cs
@@ -3,6 +3,14 @@ namespace Discord
     /// <summary>
     ///     Specifies the direction of where message(s) should be gotten from.
     /// </summary>
+    /// <remarks>
+    ///     This enum is used to specify what direction should the message retrieval go.
+    ///     <note type="important">
+    ///         At the time of writing, <see cref="Around"/> is not yet implemented into 
+    ///         <see cref="IMessageChannel.GetMessagesAsync"/>. Attempting to use the method with <see cref="Around"/>
+    ///         as its direction will throw a <see cref="System.NotImplementedException"/>.
+    ///     </note>
+    /// </remarks>
     public enum Direction
     {
         /// <summary>
