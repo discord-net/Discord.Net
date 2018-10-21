discord-net/Discord.Net

commit d30d12246d5173321c2556cf65d3c69da230cb8c

Update Guild and Message Models (#1165)
* Add ExplicitContentFilter property to Guild

* re-order properties to match order listed on api docs

* re-order SystemChannelId to match api docs

* Implement ApplicationId in Guild model

* Add ExplicitContentFilter property to Guild

* re-order properties to match order listed on api docs

* re-order SystemChannelId to match api docs

* Implement ApplicationId in Guild model

* Improve xmldoc for IGuild ExplicitContentFilter

* Update xmldoc

* docs "Id" -> "ID"

* rename Test.GuildPermissions to a more general Test.Guilds

* Add ExplicitContentFilter to GuildProperties

* Add a test for ExplicitContentFilterLevel modification behavior

* Implement ModifyAsync behavior

* simplify ExplicitContentFilter test

* Add RestGuild ApplicationId inheritdoc

* Implement message Activity and Application model update

* RestMessage Application and Activity implementation

* add ToString to MessageApplication

* Add IconUrl property to MessageApplication

* clean up whitespace

* another excessive whitespace removal

src/Discord.Net.Core/Entities/Messages/MessageActivity.cs
@@ -1,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord
+{
+    [DebuggerDisplay(@"{DebuggerDisplay,nq}")]
+    public class MessageActivity
+    {
+        /// <summary>
+        ///     Gets the type of activity of this message.
+        /// </summary>
+        public MessageActivityType Type { get; set; }
+        /// <summary>
+        ///     Gets the party ID of this activity, if any.
+        /// </summary>
+        public string PartyId { get; set; }
+
+        private string DebuggerDisplay
+            => $"{Type}{(string.IsNullOrWhiteSpace(PartyId) ? "" : $" {PartyId}")}";
+
+        public override string ToString() => DebuggerDisplay;
+    }
+}

src/Discord.Net.Core/Entities/Messages/MessageApplication.cs
@@ -1,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord
+{
+    [DebuggerDisplay(@"{DebuggerDisplay,nq}")]
+    public class MessageApplication
+    {
+        /// <summary>
+        ///     Gets the snowflake ID of the application.
+        /// </summary>
+        public ulong Id { get; set; }
+        /// <summary>
+        ///     Gets the ID of the embed's image asset.
+        /// </summary>
+        public string CoverImage { get; set; }
+        /// <summary>
+        ///     Gets the application's description.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        ///     Gets the ID of the application's icon.
+        /// </summary>
+        public string Icon { get; set; }
+        /// <summary>
+        ///     Gets the Url of the application's icon.
+        /// </summary>
+        public string IconUrl
+            => $"https://cdn.discordapp.com/app-icons/{Id}/{Icon}";
+        /// <summary>
+        ///     Gets the name of the application.
+        /// </summary>
+        public string Name { get; set; }
+        private string DebuggerDisplay
+            => $"{Name} ({Id}): {Description}";
+        public override string ToString()
+            => DebuggerDisplay;
+    }
+}

src/Discord.Net.Rest/API/Common/MessageActivity.cs
@@ -1,0 +1,17 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord.API
+{
+    public class MessageActivity
+    {
+        [JsonProperty("type")]
+        public Optional<MessageActivityType> Type { get; set; }
+        [JsonProperty("party_id")]
+        public Optional<string> PartyId { get; set; }
+    }
+}

src/Discord.Net.Rest/API/Common/MessageApplication.cs
@@ -1,0 +1,38 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord.API
+{
+    public class MessageApplication
+    {
+        /// <summary>
+        ///     Gets the snowflake ID of the application.
+        /// </summary>
+        [JsonProperty("id")]
+        public ulong Id { get; set; }
+        /// <summary>
+        ///     Gets the ID of the embed's image asset.
+        /// </summary>
+        [JsonProperty("cover_image")]
+        public string CoverImage { get; set; }
+        /// <summary>
+        ///     Gets the application's description.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        /// <summary>
+        ///     Gets the ID of the application's icon.
+        /// </summary>
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+        /// <summary>
+        ///     Gets the name of the application.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}

src/Discord.Net.Core/Entities/Guilds/ExplicitContentFilterLevel.cs
@@ -1,0 +1,13 @@
+namespace Discord
+{
+    public enum ExplicitContentFilterLevel
+    {
+        /// <summary> No messages will be scanned. </summary>
+        Disabled = 0,
+        /// <summary> Scans messages from all guild members that do not have a role. </summary>
+        /// <remarks> Recommented option for servers that use roles for trusted membership. </remarks>
+        MembersWithoutRoles = 1,
+        /// <summary> Scan messages sent by all guild members. </summary>
+        AllMembers = 2
+    }
+}

src/Discord.Net.Core/Entities/Guilds/GuildProperties.cs
@@ -66,5 +66,9 @@ namespace Discord
         ///     Gets or sets the ID of the owner of this guild.
         /// </summary>
         public Optional<ulong> OwnerId { get; set; }
+        /// <summary>
+        ///     Gets or sets the explicit content filter level of this guild.
+        /// </summary>
+        public Optional<ExplicitContentFilterLevel> ExplicitContentFilter { get; set; }
     }
 }

src/Discord.Net.Core/Entities/Guilds/IGuild.cs
@@ -53,6 +53,13 @@ namespace Discord
         /// </returns>
         VerificationLevel VerificationLevel { get; }
         /// <summary>
+        ///     Gets the level of content filtering applied to user's content in a Guild.
+        /// </summary>
+        /// <returns>
+        ///     The level of explicit content filtering.
+        /// </returns>
+        ExplicitContentFilterLevel ExplicitContentFilter { get; }
+        /// <summary>
         ///     Gets the ID of this guild's icon.
         /// </summary>
         /// <returns>
@@ -140,6 +147,13 @@ namespace Discord
         ///     A <see cref="ulong"/> representing the snowflake identifier of the user that owns this guild.
         /// </returns>
         ulong OwnerId { get; }
+        /// <summary>
+        ///     Gets the application ID of the guild creator if it is bot-created.
+        /// </summary>
+        /// <returns>
+        ///     A <see cref="ulong"/> representing the snowflake identifier of the application ID that created this guild, or <c>null</c> if it was not bot-created.
+        /// </returns>
+        ulong? ApplicationId { get; }
         /// <summary>
         ///     Gets the ID of the region hosting this guild's voice channels.
         /// </summary>

src/Discord.Net.Core/Entities/Messages/IMessage.cs
@@ -100,5 +100,25 @@ namespace Discord
         ///     A read-only collection of user IDs.
         /// </returns>
         IReadOnlyCollection<ulong> MentionedUserIds { get; }
+        /// <summary>
+        ///     Returns the Activity associated with a message.
+        /// </summary>
+        /// <remarks>
+        ///     Sent with Rich Presence-related chat embeds.
+        /// </remarks>
+        /// <returns>
+        ///     A message's activity, if any is associated.
+        /// </returns>
+        MessageActivity Activity { get; }
+        /// <summary>
+        ///     Returns the Application associated with a messsage.
+        /// </summary>
+        /// <remarks>
+        ///     Sent with Rich-Presence-related chat embeds.
+        /// </remarks>
+        /// <returns>
+        ///     A message's application, if any is associated.
+        /// </returns>
+        MessageApplication Application { get; }
     }
 }

commit 8e6e0e9ad2fe535b28ab5ed0f8e5b9e127cde092

lint: Remove public setters for MessageApplication, MessageActivity properties (#1182)
This fixes an error that was introduced in d30d12246d5173321c2556cf65d3c69da230cb8c (thanks Still for noticing!)

Changes the public property setters of the MessageActivity and MessageApplication entities to be
internal, since they cannot be modified by the user in the API.

src/Discord.Net.Core/Entities/Messages/MessageActivity.cs
@@ -13,11 +13,11 @@ namespace Discord
         /// <summary>
         ///     Gets the type of activity of this message.
         /// </summary>
-        public MessageActivityType Type { get; set; }
+        public MessageActivityType Type { get; internal set; }
         /// <summary>
         ///     Gets the party ID of this activity, if any.
         /// </summary>
-        public string PartyId { get; set; }
+        public string PartyId { get; internal set; }
 
         private string DebuggerDisplay
             => $"{Type}{(string.IsNullOrWhiteSpace(PartyId) ? "" : $" {PartyId}")}";

src/Discord.Net.Core/Entities/Messages/MessageApplication.cs
@@ -13,19 +13,19 @@ namespace Discord
         /// <summary>
         ///     Gets the snowflake ID of the application.
         /// </summary>
-        public ulong Id { get; set; }
+        public ulong Id { get; internal set; }
         /// <summary>
         ///     Gets the ID of the embed's image asset.
         /// </summary>
-        public string CoverImage { get; set; }
+        public string CoverImage { get; internal set; }
         /// <summary>
         ///     Gets the application's description.
         /// </summary>
-        public string Description { get; set; }
+        public string Description { get; internal set; }
         /// <summary>
         ///     Gets the ID of the application's icon.
         /// </summary>
-        public string Icon { get; set; }
+        public string Icon { get; internal set; }
         /// <summary>
         ///     Gets the Url of the application's icon.
         /// </summary>
@@ -34,7 +34,7 @@ namespace Discord
         /// <summary>
         ///     Gets the name of the application.
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; internal set; }
         private string DebuggerDisplay
             => $"{Name} ({Id}): {Description}";
         public override string ToString()