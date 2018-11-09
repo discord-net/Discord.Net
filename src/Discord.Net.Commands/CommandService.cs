65db9f9d64a02f2795b4075dcd785ca556348299
Casino Boyale <kayrenhi@gmail.com>
2018-11-09 19:35:13 +0000

    Changed to just checking the result type

--- src/Discord.Net.Commands/CommandService.cs
+++ src/Discord.Net.Commands/CommandService.cs
@@ -603,6 +603,8 @@ namespace Discord.Commands
             //If we get this far, at least one parse was successful. Execute the most likely overload.
             var chosenOverload = successfulParses[0];
             var result = await chosenOverload.Key.ExecuteAsync(context, chosenOverload.Value, services).ConfigureAwait(false);
+            if (!result.IsSuccess && result is ExecuteResult)
+                await _commandExecutedEvent.InvokeAsync(chosenOverload.Key.Command, context, result);
             return result;
         }
     }
