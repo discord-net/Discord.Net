f2b9548ff30eaacdb06b4ecbcfcc757b58b6c89a
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2018-11-29 10:11:56 +0000

    Replaced If w/ while loop w/ same condition

--- src/Discord.Net.WebSocket/DiscordSocketClient.cs
+++ src/Discord.Net.WebSocket/DiscordSocketClient.cs
@@ -252,7 +252,8 @@ namespace Discord.WebSocket
             _heartbeatTask = null;
 
             // Check if hearbeatTimes is not empty before dequeuing. TryDequeue can cause blocking.
-            if (!_heartbeatTimes.IsEmpty) while (_heartbeatTimes.TryDequeue(out _)) { }
+            while (!_heartbeatTimes.IsEmpty)
+                _heartbeatTimes.TryDequeue(out _);
             _lastMessageTime = 0;
 
             await _gatewayLogger.DebugAsync("Waiting for guild downloader").ConfigureAwait(false);
