6d3d906e604d530c5f1e03d2c8dc191f8b6f45e7
Christopher Felegy <foxbot@protonmail.com>
2018-12-02 13:36:54 -0500

    docs: add example of custom error message to sample

--- samples/02_commands_framework/Modules/PublicModule.cs
+++ samples/02_commands_framework/Modules/PublicModule.cs
@@ -59,5 +59,10 @@ namespace _02_commands_framework.Modules
         [Command("list")]
         public Task ListAsync(params string[] objects)
             => ReplyAsync("You listed: " + string.Join("; ", objects));
+
+        [Command("guild_only")]
+        [RequireContext(ContextType.Guild, ErrorMessage = "Sorry, this command must be ran from within a server, not a DM!")]
+        public Task GuildOnlyCommand()
+            => ReplyAsync("Nothing to see here!");
     }
 }
