f40653bcc2873fd58c6bef9fb43510bd22cc5e96
Chris Johnston <chjohnston@protonmail.com>
2019-01-12 10:23:34 -0800

    Add incomplete xmldoc to ActivityFlag
    Added xmldoc to the ActivityFlag type, excluded flags that i was unsure about

--- src/Discord.Net.Core/Entities/Activities/ActivityFlag.cs
+++ src/Discord.Net.Core/Entities/Activities/ActivityFlag.cs
@@ -2,14 +2,30 @@ using System;
 
 namespace Discord
 {
+    /// <summary>
+    ///     Flags for the <see cref="IActivity.Flags"/> property, that are ORd together.
+    ///     These describe what the activity payload includes.
+    /// </summary>
     [Flags]
     public enum ActivityFlag
     {
         Instance = 1,
+        /// <summary>
+        ///     Indicates that this activity can be joined.
+        /// </summary>
         Join = 0b10,
+        /// <summary>
+        ///     Indicates that this activity can be spectated.
+        /// </summary>
         Spectate = 0b100,
         JoinRequest = 0b1000,
+        /// <summary>
+        ///     Indicates that a user can listen along in Spotify.
+        /// </summary>
         Sync = 0b10000,
+        /// <summary>
+        ///     Indicates that a user can play this song.
+        /// </summary>
         Play = 0b100000
     }
 }
