46be8fcc89018bbed286529589755172aa66a825
Chris Johnston <chris@thejohnstons.net>
2019-01-31 16:47:35 -0800

    typo fix

--- src/Discord.Net.Core/Extensions/AttachmentExtensions.cs
+++ src/Discord.Net.Core/Extensions/AttachmentExtensions.cs
@@ -3,7 +3,7 @@ namespace Discord
     public static class AttachmentExtensions
     {
         /// <summary>
-        ///     Gets wheter the message's attachments are spoilers or not.
+        ///     Gets whether the message's attachments are spoilers or not.
         /// </summary>
         public static bool IsSpoiler(this IAttachment attachment)
             => attachment.Filename.StartsWith(IAttachment.SpoilerPrefix);
