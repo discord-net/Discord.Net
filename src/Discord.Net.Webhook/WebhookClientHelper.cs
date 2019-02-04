d1b1726cf6140622f586cef9fa5509f91146e8d1
Chris Johnston <chris@thejohnstons.net>
2019-02-04 11:03:32 -0800

    Add isSpoiler support to webhook client
    adds the isSpoiler field to uploading files with a webhook, which will only
    insert "SPOILER_" to the start of the filename. This does not include other
    fields in the payload, as this is not in the documentation, and was not observed
    like in the regular client

--- src/Discord.Net.Webhook/WebhookClientHelper.cs
+++ src/Discord.Net.Webhook/WebhookClientHelper.cs
@@ -35,16 +35,16 @@ namespace Discord.Webhook
             return model.Id;
         }
         public static async Task<ulong> SendFileAsync(DiscordWebhookClient client, string filePath, string text, bool isTTS, 
-            IEnumerable<Embed> embeds, string username, string avatarUrl, RequestOptions options)
+            IEnumerable<Embed> embeds, string username, string avatarUrl, RequestOptions options, bool isSpoiler)
         {
             string filename = Path.GetFileName(filePath);
             using (var file = File.OpenRead(filePath))
-                return await SendFileAsync(client, file, filename, text, isTTS, embeds, username, avatarUrl, options).ConfigureAwait(false);
+                return await SendFileAsync(client, file, filename, text, isTTS, embeds, username, avatarUrl, options, isSpoiler).ConfigureAwait(false);
         }
         public static async Task<ulong> SendFileAsync(DiscordWebhookClient client, Stream stream, string filename, string text, bool isTTS,
-            IEnumerable<Embed> embeds, string username, string avatarUrl, RequestOptions options)
+            IEnumerable<Embed> embeds, string username, string avatarUrl, RequestOptions options, bool isSpoiler)
         {
-            var args = new UploadWebhookFileParams(stream) { Filename = filename, Content = text, IsTTS = isTTS };
+            var args = new UploadWebhookFileParams(stream) { Filename = filename, Content = text, IsTTS = isTTS, IsSpoiler = isSpoiler };
             if (username != null)
                 args.Username = username;
             if (avatarUrl != null)
