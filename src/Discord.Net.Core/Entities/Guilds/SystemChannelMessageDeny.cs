a00d8a419f3f10e15c99dfc7a958cbc415857790
Chris Johnston <chjohnston@protonmail.com>
2019-06-04 21:04:56 -0700

    add a None flag to SystemChannelMessage enum

--- src/Discord.Net.Core/Entities/Guilds/SystemChannelMessageDeny.cs
+++ src/Discord.Net.Core/Entities/Guilds/SystemChannelMessageDeny.cs
@@ -6,6 +6,11 @@ namespace Discord
     public enum SystemChannelMessageDeny
     {
         /// <summary>
+        ///     Deny none of the system channel messages.
+        ///     This will enable all of the system channel messages.
+        /// </summary>
+        None = 0,
+        /// <summary>
         ///     The messages that are sent when a user joins the guild.
         /// </summary>
         WelcomeMessage = 0b1,
