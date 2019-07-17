574191bbe29f42551af5df71d467f489b9fbddb5
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-07-17 17:58:34 +0800

    Fix formatting for first bot token sample

--- docs/guides/getting_started/samples/first-bot/client.cs
+++ docs/guides/getting_started/samples/first-bot/client.cs
@@ -2,22 +2,22 @@ private DiscordSocketClient _client;
 
 public async Task MainAsync()
 {
-	_client = new DiscordSocketClient();
+    _client = new DiscordSocketClient();
 
-	_client.Log += Log;
-    
-    // You can assign your bot token to a string, and pass that in to connect.
-    // This however is insecure, particularly if you plan to have your code hosted in a repository.
+    _client.Log += Log;
+
+    //  You can assign your bot token to a string, and pass that in to connect.
+    //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
     var token = "token";
-    
-    // Some alternative  options would be to keep your token in an Environment Variable or a standalone file.
+
+    // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
     // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
     // var token = File.ReadAllText("token.txt");
     // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
 
-	await _client.LoginAsync(TokenType.Bot, token);
-	await _client.StartAsync();
+    await _client.LoginAsync(TokenType.Bot, token);
+    await _client.StartAsync();
 
-	// Block this task until the program is closed.
-	await Task.Delay(-1);
+    // Block this task until the program is closed.
+    await Task.Delay(-1);
 }
