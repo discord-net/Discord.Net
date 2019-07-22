dd3954071433a1c10f42e8510bb7961d20a089d1
Christopher Felegy <foxbot@protonmail.com>
2019-07-21 21:09:35 -0400

    install docfx.console from NuGetCommand

--- azure/docs.bat
+++ azure/docs.bat
@@ -1,7 +1,7 @@
-nuget install docfx.console
-git clone https://github.com/discord-net/docs-static.git
-RMDIR /S docs-static/latest
-docfx.console/tools/docfx.exe docs/docfx.json -o docs-static/latest/
-git -C docs-static add -A
-git -C docs-static commit -m "[ci deploy] %date% %time%: %Build.BuildId%"
-git -C docs-static push --force
+git clone https://github.com/discord-net/docs-static.git || EXIT /B 1
+RMDIR /S docs-static/latest || EXIT /B 1
+docfx.console/tools/docfx.exe docs/docfx.json -o docs-static/latest/ || EXIT /B 1
+git config --global user.name "Discord.Net CI Robot" && git config --global user.email "robot@foxbot.me"
+git -C docs-static add -A || EXIT /B 1
+git -C docs-static commit -m "[ci deploy] %date% %time%: %Build.BuildId%" || EXIT /B 1
+git -C docs-static push --force || EXIT /B 1
