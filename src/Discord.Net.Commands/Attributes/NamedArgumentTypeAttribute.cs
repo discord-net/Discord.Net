704e040a5257ee75835d3581c857c905dcd05563
LtLi0n <31516441+LtLi0n@users.noreply.github.com>
2019-07-30 22:02:23 +0300

    Fixed a spelling error.

--- src/Discord.Net.Commands/Attributes/NamedArgumentTypeAttribute.cs
+++ src/Discord.Net.Commands/Attributes/NamedArgumentTypeAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace Discord.Commands
 {
     /// <summary>
-    /// Instructs the command system to treat command paramters of this type
+    /// Instructs the command system to treat command parameters of this type
     /// as a collection of named arguments matching to its properties.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
