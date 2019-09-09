35b25f2d3d3562ef4d5ebb26f2e1ff6c653151af
NeKz <nekzor@users.noreply.github.com>
2019-09-09 22:25:06 +0200

    Remove AvatarHash from MemberInfo

--- src/Discord.Net.Rest/Entities/AuditLogs/DataTypes/MemberInfo.cs
+++ src/Discord.Net.Rest/Entities/AuditLogs/DataTypes/MemberInfo.cs
@@ -2,17 +2,15 @@ namespace Discord.Rest
 {
     public struct MemberInfo
     {
-        internal MemberInfo(string nick, bool? deaf, bool? mute, string avatar_hash)
+        internal MemberInfo(string nick, bool? deaf, bool? mute)
         {
             Nickname = nick;
             Deaf = deaf;
             Mute = mute;
-            AvatarHash = avatar_hash;
         }
 
         public string Nickname { get; }
         public bool? Deaf { get; }
         public bool? Mute { get; }
-        public string AvatarHash { get; }
     }
 }
