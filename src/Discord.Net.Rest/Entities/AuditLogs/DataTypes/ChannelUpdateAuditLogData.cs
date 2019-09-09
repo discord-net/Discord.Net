c34eb4ff57912e5791bed6ab8e98bc6f430e9fc8
NeKz <nekzor@users.noreply.github.com>
2019-09-09 22:18:44 +0200

    Add missing properties to ChannelInfo
    Remove UserLimit property

--- src/Discord.Net.Rest/Entities/AuditLogs/DataTypes/ChannelUpdateAuditLogData.cs
+++ src/Discord.Net.Rest/Entities/AuditLogs/DataTypes/ChannelUpdateAuditLogData.cs
@@ -23,20 +23,23 @@ namespace Discord.Rest
 
             var nameModel = changes.FirstOrDefault(x => x.ChangedProperty == "name");
             var topicModel = changes.FirstOrDefault(x => x.ChangedProperty == "topic");
+            var rateLimitPerUserModel = changes.FirstOrDefault(x => x.ChangedProperty == "rate_limit_per_user");
+            var nsfwModel = changes.FirstOrDefault(x => x.ChangedProperty == "nsfw");
             var bitrateModel = changes.FirstOrDefault(x => x.ChangedProperty == "bitrate");
-            var userLimitModel = changes.FirstOrDefault(x => x.ChangedProperty == "user_limit");
 
             string oldName = nameModel?.OldValue?.ToObject<string>(discord.ApiClient.Serializer),
                 newName = nameModel?.NewValue?.ToObject<string>(discord.ApiClient.Serializer);
             string oldTopic = topicModel?.OldValue?.ToObject<string>(discord.ApiClient.Serializer),
                 newTopic = topicModel?.NewValue?.ToObject<string>(discord.ApiClient.Serializer);
+            int? oldRateLimitPerUser = rateLimitPerUserModel?.OldValue?.ToObject<int>(discord.ApiClient.Serializer),
+                newRateLimitPerUser = rateLimitPerUserModel?.NewValue?.ToObject<int>(discord.ApiClient.Serializer);
+            bool? oldNsfw = nsfwModel?.OldValue?.ToObject<bool>(discord.ApiClient.Serializer),
+                newNsfw = nsfwModel?.NewValue?.ToObject<bool>(discord.ApiClient.Serializer);
             int? oldBitrate = bitrateModel?.OldValue?.ToObject<int>(discord.ApiClient.Serializer),
                 newBitrate = bitrateModel?.NewValue?.ToObject<int>(discord.ApiClient.Serializer);
-            int? oldLimit = userLimitModel?.OldValue?.ToObject<int>(discord.ApiClient.Serializer),
-                newLimit = userLimitModel?.NewValue?.ToObject<int>(discord.ApiClient.Serializer);
 
-            var before = new ChannelInfo(oldName, oldTopic, oldBitrate, oldLimit);
-            var after = new ChannelInfo(newName, newTopic, newBitrate, newLimit);
+            var before = new ChannelInfo(oldName, oldTopic, oldRateLimitPerUser, oldNsfw, oldBitrate);
+            var after = new ChannelInfo(newName, newTopic, newRateLimitPerUser, newNsfw, newBitrate);
 
             return new ChannelUpdateAuditLogData(entry.TargetId.Value, before, after);
         }
