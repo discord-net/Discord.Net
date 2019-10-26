135733dea9e1756218238556f12e0d8d3b409a27
Chris Johnston <chjohnston@protonmail.com>
2019-10-26 01:16:10 -0700

    Implement CustomStatusGame activity
    Adds the CustomStatusGame class, which is the activity corresponding to the custom status feature.

--- src/Discord.Net.Rest/API/Common/Game.cs
+++ src/Discord.Net.Rest/API/Common/Game.cs
@@ -1,6 +1,7 @@
 #pragma warning disable CS1591
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System;
 using System.Runtime.Serialization;
 
 namespace Discord.API
@@ -35,6 +36,12 @@ namespace Discord.API
         public Optional<string> SessionId { get; set; }
         [JsonProperty("Flags")]
         public Optional<ActivityProperties> Flags { get; set; }
+        [JsonProperty("id")]
+        public Optional<string> Id { get; set; }
+        [JsonProperty("emoji")]
+        public Optional<Emoji> Emoji { get; set; }
+        [JsonProperty("created_at")]
+        public Optional<long> CreatedAt { get; set; }
 
         [OnError]
         internal void OnError(StreamingContext context, ErrorContext errorContext)
