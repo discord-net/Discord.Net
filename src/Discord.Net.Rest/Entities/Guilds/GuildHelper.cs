ff8cb63f9fb5432330f3532aa714fd549d0b78eb
LtLi0n <LtLi0n@users.noreply.github.com>
2019-11-06 01:30:31 +0200

    Fixed GetUsersAsync to use MaxUsersPerBatch const as limit instead of MaxMessagesPerBatch.
    Requests are now returning up to 1000 guild user entities instead of the previous 100.

--- src/Discord.Net.Rest/Entities/Guilds/GuildHelper.cs
+++ src/Discord.Net.Rest/Entities/Guilds/GuildHelper.cs
@@ -343,7 +343,7 @@ namespace Discord.Rest
             ulong? fromUserId, int? limit, RequestOptions options)
         {
             return new PagedAsyncEnumerable<RestGuildUser>(
-                DiscordConfig.MaxMessagesPerBatch,
+                DiscordConfig.MaxUsersPerBatch,
                 async (info, ct) =>
                 {
                     var args = new GetGuildMembersParams
@@ -357,7 +357,7 @@ namespace Discord.Rest
                 },
                 nextPage: (info, lastPage) =>
                 {
-                    if (lastPage.Count != DiscordConfig.MaxMessagesPerBatch)
+                    if (lastPage.Count != DiscordConfig.MaxUsersPerBatch)
                         return false;
                     info.Position = lastPage.Max(x => x.Id);
                     return true;
