f83657dab860ea07d88dbd69dc2594e91e9544c7
Chris Johnston <chris@thejohnstons.net>
2020-03-03 23:04:24 -0800

    lint: remove commented-out code
    This change removes the commented-out code which was added during testing from the previous commit.

--- src/Discord.Net.WebSocket/Entities/Channels/SocketDMChannel.cs
+++ src/Discord.Net.WebSocket/Entities/Channels/SocketDMChannel.cs
@@ -235,8 +235,6 @@ namespace Discord.WebSocket
         async Task<IUserMessage> IMessageChannel.SendFileAsync(Stream stream, string filename, string text, bool isTTS, Embed embed, RequestOptions options, bool isSpoiler)
             => await SendFileAsync(stream, filename, text, isTTS, embed, options, isSpoiler).ConfigureAwait(false);
         /// <inheritdoc />
-        //async Task<IUserMessage> IMessageChannel.SendMessageAsync(string text, bool isTTS, Embed embed, RequestOptions options)
-        //    => await SendMessageAsync(text, isTTS, embed, null, options).ConfigureAwait(false);
         async Task<IUserMessage> IMessageChannel.SendMessageAsync(string text, bool isTTS, Embed embed, AllowedMentions allowedMentions, RequestOptions options)
             => await SendMessageAsync(text, isTTS, embed, allowedMentions, options).ConfigureAwait(false);
 
