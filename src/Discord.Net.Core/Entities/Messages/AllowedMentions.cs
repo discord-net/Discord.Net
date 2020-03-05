89652809d481bd8e313ceaf842edc72bd0d2efeb
Chris Johnston <chris@thejohnstons.net>
2020-03-04 22:44:05 -0800

    docs: reword summaries for AllowedMentions type

--- src/Discord.Net.Core/Entities/Messages/AllowedMentions.cs
+++ src/Discord.Net.Core/Entities/Messages/AllowedMentions.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 namespace Discord
 {
     /// <summary>
-    ///     Defines which mentions and types of mentions that will notify users from the message text.
+    ///     Defines which mentions and types of mentions that will notify users from the message content.
     /// </summary>
     public class AllowedMentions
     {
         /// <summary>
-        ///     Gets or sets the type of mentions that will notify users.
-        ///     If <c>null</c>, only the role and user Ids specified in <see cref="RoleIds"/>
-        ///     and <see cref="UserIds"/> respectively will be notified.
+        ///     Gets or sets the type of mentions that will be parsed from the message content.
+        ///     The <see cref="AllowedMentionTypes.Users"/> flag is mutually exclusive with the <see cref="UserIds"/>
+        ///     property, and the <see cref="AllowedMentionTypes.Roles"/> flag is mutually exclusive with the
+        ///     <see cref="RoleIds"/> property.
         /// </summary>
         public AllowedMentionTypes? AllowedTypes { get; set; }
 
         /// <summary>
         ///     Gets or sets the list of all role Ids that will be mentioned.
+        ///     This property is mutually exclusive with the <see cref="AllowedMentionTypes.Roles"/>
+        ///     flag of the <see cref="AllowedTypes"/> property. If the flag is set, the value of this property
+        ///     must be <c>null</c> or empty.
         /// </summary>
         public List<ulong> RoleIds { get; set; }
 
         /// <summary>
         ///     Gets or sets the list of all user Ids that will be mentioned.
+        ///     This property is mutually exclusive with the <see cref="AllowedMentionTypes.Users"/>
+        ///     flag of the <see cref="AllowedTypes"/> property. If the flag is set, the value of this property
+        ///     must be <c>null</c> or empty.
         /// </summary>
         public List<ulong> UserIds { get; set; }
     }
