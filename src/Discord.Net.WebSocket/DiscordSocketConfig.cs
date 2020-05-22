62539f039109976ea64c52ec7a8df055b47e31e4
Still Hsu <5843208+Still34@users.noreply.github.com>
2020-05-22 11:42:29 +0800

    Move bulk deletes remarks from <summary> to <remarks>

--- src/Discord.Net.WebSocket/DiscordSocketConfig.cs
+++ src/Discord.Net.WebSocket/DiscordSocketConfig.cs
@@ -109,14 +109,18 @@ namespace Discord.WebSocket
 
         /// <summary>
         ///     Gets or sets the behavior for <see cref="BaseSocketClient.MessageDeleted"/> on bulk deletes.
-        ///
-        ///     If true, the <see cref="BaseSocketClient.MessageDeleted"/> event will not be raised for bulk deletes, and
-        ///     only the <see cref="BaseSocketClient.MessagesBulkDeleted"/> will be raised.
-        ///
-        ///     If false, both events will be raised.
-        ///
-        ///     If unset, both events will be raised, but a warning will be raised the first time a bulk delete event is received.
         /// </summary>
+        /// <remarks>
+        /// <para>
+        ///     If <c>true</c>, the <see cref="BaseSocketClient.MessageDeleted"/> event will not be raised for bulk
+        ///     deletes, and only the <see cref="BaseSocketClient.MessagesBulkDeleted"/> will be raised. If <c>false</c>
+        ///     , both events will be raised.
+        /// </para>
+        /// <para>
+        ///     If unset, both events will be raised, but a warning will be raised the first time a bulk delete event is
+        ///     received.
+        /// </para>
+        /// </remarks>
         public bool? ExclusiveBulkDelete { get; set; } = null;
 
         /// <summary>
