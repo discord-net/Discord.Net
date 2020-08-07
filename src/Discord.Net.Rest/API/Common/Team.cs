3cde9cee0ac5929dcdcc77a0970e608f7aa8b690
Paulo <pnmanjos@hotmail.com>
2020-08-07 03:22:45 -0300

    Add missing application properties
    Add IsBotPublic, BotRequiresCodeGrant, and Team properties to IApplication

---
+++ src/Discord.Net.Rest/API/Common/Team.cs
@@ -0,0 +1,17 @@
+#pragma warning disable CS1591
+using Newtonsoft.Json;
+
+namespace Discord.API
+{
+    internal class Team
+    {
+        [JsonProperty("icon")]
+        public Optional<string> Icon { get; set; }
+        [JsonProperty("id")]
+        public ulong Id { get; set; }
+        [JsonProperty("members")]
+        public TeamMember[] TeamMembers { get; set; }
+        [JsonProperty("owner_user_id")]
+        public ulong OwnerUserId { get; set; }
+    }
+}
