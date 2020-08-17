cd69874f3f25f6f429c1b8a02e92f6409f7e20ee
Paulo <pnmanjos@hotmail.com>
2020-08-17 09:33:38 -0300

    Let's not forget another one

--- src/Discord.Net.Rest/Entities/Channels/ChannelHelper.cs
+++ src/Discord.Net.Rest/Entities/Channels/ChannelHelper.cs
@@ -28,7 +28,16 @@ namespace Discord.Rest
             {
                 Name = args.Name,
                 Position = args.Position,
-                CategoryId = args.CategoryId
+                CategoryId = args.CategoryId,
+                Overwrites = args.PermissionOverwrites.IsSpecified
+                    ? args.PermissionOverwrites.Value.Select(overwrite => new API.Overwrite
+                    {
+                        TargetId = overwrite.TargetId,
+                        TargetType = overwrite.TargetType,
+                        Allow = overwrite.Permissions.AllowValue,
+                        Deny = overwrite.Permissions.DenyValue
+                    }).ToArray()
+                    : Optional.Create<API.Overwrite[]>(),
             };
             return await client.ApiClient.ModifyGuildChannelAsync(channel.Id, apiArgs, options).ConfigureAwait(false);
         }
