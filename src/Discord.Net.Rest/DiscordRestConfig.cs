64514661a7517bb417fd83a0aa4656525e5bfec1
emorell96 <emorell96@hotmail.com>
2021-03-23 22:35:20 -0700

    added ClientId, and ClientSecret. They default to null to avoid incompatibilities with previous code.

--- src/Discord.Net.Rest/DiscordRestConfig.cs
+++ src/Discord.Net.Rest/DiscordRestConfig.cs
@@ -9,5 +9,15 @@ namespace Discord.Rest
     {
         /// <summary> Gets or sets the provider used to generate new REST connections. </summary>
         public RestClientProvider RestClientProvider { get; set; } = DefaultRestClientProvider.Instance;
+
+        /// <summary>
+        /// The client Id provided by Discord. Provide this if you want to use the refresh token features. Else they will throw errors if you atempt to use them!!
+        /// </summary>
+        public string ClientId { get; set; } = null;
+        /// <summary>
+        /// The client secret provided by Discord. Provide this if you want to use the refresh token features. Else they will throw errors if you atempt to use them!!
+        /// </summary>
+        public string ClientSecret { get; set; } = null;
     }
+    
 }
