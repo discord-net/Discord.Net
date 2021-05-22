4b8d4441c5ca2afe1d40aa0406025b3f7c5127ea
Max <73736326+Max2408@users.noreply.github.com>
2021-05-22 23:00:08 +0530

    feature: Add Discord Certified Moderator user flag (#1844)
    This pull request adds the Discord Certified Moderator badge flag to the User Flags
    PR in discord-api-docs: https://github.com/discord/discord-api-docs/pull/2946

--- src/Discord.Net.Core/Entities/Users/UserProperties.cs
+++ src/Discord.Net.Core/Entities/Users/UserProperties.cs
@@ -67,5 +67,9 @@ namespace Discord
         ///     Flag given to users that developed bots and early verified their accounts.
         /// </summary>
         EarlyVerifiedBotDeveloper = 1 << 17,
+         /// <summary>
+        ///     Flag given to users that are discord certified moderators who has give discord's exam.
+        /// </summary>
+        DiscordCertifiedModerator = 1 << 18,
     }
 }
