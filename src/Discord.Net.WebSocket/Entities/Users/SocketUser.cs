3888da044a3f78dba10dcc9a470b1a8efb96f60d
Paulo <pnmanjos@hotmail.com>
2021-05-26 17:22:22 -0300

    Remove DM cache and fix references

--- src/Discord.Net.WebSocket/Entities/Users/SocketUser.cs
+++ src/Discord.Net.WebSocket/Entities/Users/SocketUser.cs
@@ -92,8 +92,8 @@ namespace Discord.WebSocket
         }
 
         /// <inheritdoc />
-        public async Task<IDMChannel> GetOrCreateDMChannelAsync(RequestOptions options = null)
-            => GlobalUser.DMChannel ?? await UserHelper.CreateDMChannelAsync(this, Discord, options).ConfigureAwait(false) as IDMChannel;
+        public async Task<IDMChannel> CreateDMChannelAsync(RequestOptions options = null)
+            => await UserHelper.CreateDMChannelAsync(this, Discord, options).ConfigureAwait(false);
 
         /// <inheritdoc />
         public string GetAvatarUrl(ImageFormat format = ImageFormat.Auto, ushort size = 128)
