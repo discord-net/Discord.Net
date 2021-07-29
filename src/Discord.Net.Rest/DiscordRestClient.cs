71cfb2da6a79e121f1bd7a390dc9ffe6bc123283
Henry <emorell96@hotmail.com>
2021-07-29 13:31:51 -0700

    updated the example to use the latest

--- src/Discord.Net.Rest/DiscordRestClient.cs
+++ src/Discord.Net.Rest/DiscordRestClient.cs
@@ -31,7 +31,6 @@ namespace Discord.Rest
         private static API.DiscordRestApiClient CreateApiClient(DiscordRestConfig config)
             => new API.DiscordRestApiClient(config.RestClientProvider,
                 DiscordRestConfig.UserAgent,
-                rateLimitPrecision: config.RateLimitPrecision,
                 useSystemClock: config.UseSystemClock,
                 clientId: config.ClientId,
                 clientSecret: config.ClientSecret);
