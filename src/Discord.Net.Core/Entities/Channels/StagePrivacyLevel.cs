ae729ef3261aefb326586edeec4cf3f23d484d1b
quin lynch <lynchquin@gmail.com>
2021-10-07 01:49:29 -0300

    Update StagePrivacyLevel summary

--- src/Discord.Net.Core/Entities/Channels/StagePrivacyLevel.cs
+++ src/Discord.Net.Core/Entities/Channels/StagePrivacyLevel.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Discord
 {
+    /// <summary>
+    ///     Represents the privacy level of a stage.
+    /// </summary>
     public enum StagePrivacyLevel
     {
+        /// <summary>
+        ///     The stage is a public stage.
+        /// </summary>
         Public = 1,
+
+        /// <summary>
+        ///     The stage is non public and is only accessable from the guild.
+        /// </summary>
         GuildOnly = 2,
     }
 }
