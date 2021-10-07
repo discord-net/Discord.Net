e8edd2a45087b53e301d3e702693cb64c674cf76
quin lynch <lynchquin@gmail.com>
2021-10-07 01:13:08 -0300

    Fix summary in IApplicationCommandOptionChoice

--- src/Discord.Net.Core/Entities/Interactions/IApplicationCommandOptionChoice.cs
+++ src/Discord.Net.Core/Entities/Interactions/IApplicationCommandOptionChoice.cs
@@ -12,12 +12,12 @@ namespace Discord
     public interface IApplicationCommandOptionChoice
     {
         /// <summary>
-        ///     1-100 character choice name.
+        ///    Gets the name of this choice.
         /// </summary>
         string Name { get; }
 
         /// <summary>
-        ///     value of the choice.
+        ///     Gets the value of the choice.
         /// </summary>
         object Value { get; }
 
