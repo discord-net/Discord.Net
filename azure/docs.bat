ec0c3e2edf6f7da38ad82c95142740d9c5e1f5ec
quin lynch <lynchquin@gmail.com>
2021-10-17 09:05:32 -0300

    Rename docs repo for clone

--- azure/docs.bat
+++ azure/docs.bat
@@ -1,5 +1,5 @@
 ECHO clone docs-static
-git clone git@github.com:Discord-Net-Labs/docs.git || EXIT /B 1
+git clone git@github.com:Discord-Net-Labs/docs-static.git || EXIT /B 1
 
 ECHO remove old 'latest'
 ECHO Y | RMDIR /S docs-static\latest || EXIT /B 1
@@ -9,7 +9,7 @@ docfx.console\tools\docfx.exe docs/docfx.json -o docs-staging || EXIT /B 1
 ROBOCOPY docs-staging\_site docs-static\latest /MIR
 
 ECHO commit and deploy
-git config --global user.name "Discord.Net CI Robot" && git config --global user.email "robot@foxbot.me"
+git config --global user.name "Discord.Net Labs CI Robot" && git config --global user.email "robot@discord-net-labs.com"
 git -C docs-static add -A || EXIT /B 1
 git -C docs-static commit -m "[ci deploy] %date% %time%: %Build.BuildId%" || EXIT /B 1
 git -C docs-static push --force || EXIT /B 1
