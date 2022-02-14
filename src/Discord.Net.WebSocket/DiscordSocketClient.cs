c6013f17892c7ff7835c9d50de0b1b5fc0f25cf7
Armano den Boef <t.a.denboef@gmail.com>
2022-02-14 17:03:06 +0100

    Swap out for any check

--- src/Discord.Net.WebSocket/DiscordSocketClient.cs
+++ src/Discord.Net.WebSocket/DiscordSocketClient.cs
@@ -152,7 +152,7 @@ namespace Discord.WebSocket
             LogGatewayIntentWarnings = config.LogGatewayIntentWarnings;
             HandlerTimeout = config.HandlerTimeout;
             State = new ClientState(0, 0);
-            if (shardedClient == null || shardedClient.Shards.First()?.ShardId == ShardId)
+            if (shardedClient == null || !shardedClient.Shards.Any())
                 Rest = new DiscordSocketRestClient(config, ApiClient);
             _heartbeatTimes = new ConcurrentQueue<long>();
             _gatewayIntents = config.GatewayIntents;
