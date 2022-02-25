3e1ccdba0b7b04887402358566837b5ced97e819
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2022-02-25 22:29:27 +0300

    move concrete impl of IRouteSegmentMatch to internal

--- src/Discord.Net.Core/Interactions/RouteSegmentMatch.cs
+++ src/Discord.Net.Core/Interactions/RouteSegmentMatch.cs
@@ -3,12 +3,12 @@ namespace Discord
     /// <summary>
     ///     Represents an object for storing CustomId wild card matches.
     /// </summary>
-    public record RouteSegmentMatch : IRouteSegmentMatch
+    internal record RouteSegmentMatch : IRouteSegmentMatch
     {
         /// <inheritdoc/>
         public string Value { get; }
 
-        internal RouteSegmentMatch(string value)
+        public RouteSegmentMatch(string value)
         {
             Value = value;
         }
