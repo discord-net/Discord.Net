9ba64f62d1013387231a98c00dc5d1dbb86bdb23
Quin Lynch <49576606+quinchs@users.noreply.github.com>
2022-03-02 19:23:39 -0400

    Interaction Service Complex Parameters (#2155)
    * Interaction Service Complex Parameters
    
    * add complex parameters
    
    * add complex parameters
    
    * fix build errors
    
    * add argument parsing
    
    * add nested complex parameter checks
    
    * add inline docs
    
    * add preferred constructor declaration
    
    * fix autocompletehandlers for complex parameters
    
    * make GetConstructor private
    
    * use flattened params in ToProps method
    
    * make DiscordType of SlashParameter nullable
    
    * add docs to Flattened parameters collection and move the GetComplexParameterCtor method
    
    * add inline docs to SlashCommandParameterBuilder.ComplexParameterFields
    
    * add check for validating required/optinal parameter order
    
    * implement change requests
    
    * return internal ParseResult as ExecuteResult
    
    Co-Authored-By: Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
    
    * fix merge errors
    
    Co-authored-by: Cenk Ergen <57065323+Cenngo@users.noreply.github.com>

--- src/Discord.Net.Interactions/Info/Commands/SlashCommandInfo.cs
+++ src/Discord.Net.Interactions/Info/Commands/SlashCommandInfo.cs
@@ -13,6 +13,8 @@ namespace Discord.Interactions
     /// </summary>
     public class SlashCommandInfo : CommandInfo<SlashCommandParameterInfo>, IApplicationCommandInfo
     {
+        internal IReadOnlyDictionary<string, SlashCommandParameterInfo> _flattenedParameterDictionary { get; }
+
         /// <summary>
         ///     Gets the command description that will be displayed on Discord.
         /// </summary>
@@ -30,11 +32,23 @@ namespace Discord.Interactions
         /// <inheritdoc/>
         public override bool SupportsWildCards => false;
 
+        /// <summary>
+        ///     Gets the flattened collection of command parameters and complex parameter fields.
+        /// </summary>
+        public IReadOnlyCollection<SlashCommandParameterInfo> FlattenedParameters { get; }
+
         internal SlashCommandInfo (Builders.SlashCommandBuilder builder, ModuleInfo module, InteractionService commandService) : base(builder, module, commandService)
         {
             Description = builder.Description;
             DefaultPermission = builder.DefaultPermission;
             Parameters = builder.Parameters.Select(x => x.Build(this)).ToImmutableArray();
+            FlattenedParameters = FlattenParameters(Parameters).ToImmutableArray();
+
+            for (var i = 0; i < FlattenedParameters.Count - 1; i++)
+                if (!FlattenedParameters.ElementAt(i).IsRequired && FlattenedParameters.ElementAt(i + 1).IsRequired)
+                    throw new InvalidOperationException("Optional parameters must appear after all required parameters, ComplexParameters with optional parameters must be located at the end.");
+
+            _flattenedParameterDictionary = FlattenedParameters?.ToDictionary(x => x.Name, x => x).ToImmutableDictionary();
         }
 
         /// <inheritdoc/>
@@ -56,45 +70,81 @@ namespace Discord.Interactions
         {
             try
             {
-                if (paramList?.Count() < argList?.Count())
-                    return ExecuteResult.FromError(InteractionCommandError.BadArgs ,"Command was invoked with too many parameters");
-
                 var args = new object[paramList.Count()];
 
                 for (var i = 0; i < paramList.Count(); i++)
                 {
                     var parameter = paramList.ElementAt(i);
 
-                    var arg = argList?.Find(x => string.Equals(x.Name, parameter.Name, StringComparison.OrdinalIgnoreCase));
+                    var result = await ParseArgument(parameter, context, argList, services).ConfigureAwait(false);
 
-                    if (arg == default)
+                    if(!result.IsSuccess)
                     {
-                        if (parameter.IsRequired)
-                            return ExecuteResult.FromError(InteractionCommandError.BadArgs, "Command was invoked with too few parameters");
-                        else
-                            args[i] = parameter.DefaultValue;
+                        var execResult = ExecuteResult.FromError(result);
+                        await InvokeModuleEvent(context, execResult).ConfigureAwait(false);
+                        return execResult;
                     }
+
+                    if (result is ParseResult parseResult)
+                        args[i] = parseResult.Value;
                     else
-                    {
-                        var typeConverter = parameter.TypeConverter;
-
-                        var readResult = await typeConverter.ReadAsync(context, arg, services).ConfigureAwait(false);
-
-                        if (!readResult.IsSuccess)
-                        {
-                            await InvokeModuleEvent(context, readResult).ConfigureAwait(false);
-                            return readResult;
-                        }
-
-                        args[i] = readResult.Value;
-                    }
+                        return ExecuteResult.FromError(InteractionCommandError.BadArgs, "Command parameter parsing failed for an unknown reason.");
                 }
 
                 return await RunAsync(context, args, services).ConfigureAwait(false);
             }
             catch (Exception ex)
             {
-                return ExecuteResult.FromError(ex);
+                var result = ExecuteResult.FromError(ex);
+                await InvokeModuleEvent(context, result).ConfigureAwait(false);
+                return result;
+            }
+        }
+
+        private async Task<IResult> ParseArgument(SlashCommandParameterInfo parameterInfo, IInteractionContext context, List<IApplicationCommandInteractionDataOption> argList,
+             IServiceProvider services)
+        {
+            if (parameterInfo.IsComplexParameter)
+            {
+                var ctorArgs = new object[parameterInfo.ComplexParameterFields.Count];
+
+                for (var i = 0; i < ctorArgs.Length; i++)
+                {
+                    var result = await ParseArgument(parameterInfo.ComplexParameterFields.ElementAt(i), context, argList, services).ConfigureAwait(false);
+
+                    if (!result.IsSuccess)
+                        return result;
+
+                    if (result is ParseResult parseResult)
+                        ctorArgs[i] = parseResult.Value;
+                    else
+                        return ExecuteResult.FromError(InteractionCommandError.BadArgs, "Complex command parsing failed for an unknown reason.");
+                }
+
+                return ParseResult.FromSuccess(parameterInfo._complexParameterInitializer(ctorArgs));
+            }
+            else
+            {
+                var arg = argList?.Find(x => string.Equals(x.Name, parameterInfo.Name, StringComparison.OrdinalIgnoreCase));
+
+                if (arg == default)
+                {
+                    if (parameterInfo.IsRequired)
+                        return ExecuteResult.FromError(InteractionCommandError.BadArgs, "Command was invoked with too few parameters");
+                    else
+                        return ParseResult.FromSuccess(parameterInfo.DefaultValue);
+                }
+                else
+                {
+                    var typeConverter = parameterInfo.TypeConverter;
+
+                    var readResult = await typeConverter.ReadAsync(context, arg, services).ConfigureAwait(false);
+
+                    if (!readResult.IsSuccess)
+                        return readResult;
+
+                    return ParseResult.FromSuccess(readResult.Value);
+                }
             }
         }
 
@@ -107,6 +157,16 @@ namespace Discord.Interactions
                 return $"Slash Command: \"{base.ToString()}\" for {context.User} in {context.Guild}/{context.Channel}";
             else
                 return $"Slash Command: \"{base.ToString()}\" for {context.User} in {context.Channel}";
+        }
+
+        private static IEnumerable<SlashCommandParameterInfo> FlattenParameters(IEnumerable<SlashCommandParameterInfo> parameters)
+        {
+            foreach (var parameter in parameters)
+                if (!parameter.IsComplexParameter)
+                    yield return parameter;
+                else
+                    foreach(var complexParameterField in parameter.ComplexParameterFields)
+                        yield return complexParameterField;
         }
     }
 }
