7b9fe752a95ddac08b69165d5365ef618de1d471
Quin Lynch <lynchquin@gmail.com>
2022-03-02 19:00:39 -0400

    fix renamed folder issue

--- samples/InteractionFramework/ExampleEnum.cs
+++ samples/InteractionFramework/ExampleEnum.cs
@@ -1,10 +1,20 @@
-namespace InteractionFramework
+using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_interactions_framework
 {
     public enum ExampleEnum
     {
         First,
         Second,
         Third,
-        Fourth
+        Fourth,
+        [ChoiceDisplay("Twenty First")]
+        TwentyFirst
     }
 }
