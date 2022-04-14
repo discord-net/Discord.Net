5648c2f711ca6a127b330baeb38edc07aa13a57c
Misha133 <mihagribkov133@gmail.com>
2022-04-15 00:45:39 +0300

    added some comments t othe example code

--- docs/guides/int_framework/samples/intro/groupmodule.cs
+++ docs/guides/int_framework/samples/intro/groupmodule.cs
@@ -1,13 +1,19 @@
+//You can put commands in groups
 [Group("group-name", "Group description")]
 public class CommandGroupModule : InteractionModuleBase<SocketInteractionContext>
 {
+    //This command will look like
+    //  group-name ping
     [SlashCommand("ping", "Get a pong")]
     public async Task PongSubcommand()
         => await RespondAsync("Pong!");
-
+    
+    //And even in sub-command groups
     [Group("subcommand-group-name", "Subcommand group description")]
-    public class SubcommandGroupModule : InteractionModuleBase<SocketInteractionContext>
+    public class SubСommandGroupModule : InteractionModuleBase<SocketInteractionContext>
     {
+        //This command will look like
+        //  group-name subcommand-group-name echo
         [SlashCommand("echo", "Echo an input")]
         public async Task PongSubcommand(string input)
             => await RespondAsync(input);
