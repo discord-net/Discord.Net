48712ad650e07537993caf73414cade8e5b4e016
Cenngo <cenk.ergen1@gmail.com>
2022-04-17 17:36:13 +0300

    move deconstruct extensions method to extensions folder

--- src/Discord.Net.Core/Entities/Interactions/ApplicationCommandOption.cs
+++ src/Discord.Net.Core/Entities/Interactions/ApplicationCommandOption.cs
@@ -146,13 +146,4 @@ namespace Discord
             }
         }
     }
-
-    public static class Test
-    {
-        public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> kvp, out T1 key, out T2 value)
-        {
-            key = kvp.Key;
-            value = kvp.Value;
-        }
-    }
 }
---
+++ src/Discord.Net.Core/Extensions/GenericCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Collections.Generic;
+
+public static class GenericCollectionExtensions
+{
+    public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> kvp, out T1 value1, out T2 value2)
+    {
+        value1 = kvp.Key;
+        value2 = kvp.Value;
+    }
+}
