e1a8ecd723ec3c73706cbca88d606b4bd8d8825d
Discord-NET-Robot <95661365+Discord-NET-Robot@users.noreply.github.com>
2022-04-18 04:00:58 -0300

    [Robot] Add missing json error (#2237)
    * Add 10087, 30047, 30048, 40061 Error codes
    
    * Update src/Discord.Net.Core/DiscordErrorCode.cs
    
    * Update src/Discord.Net.Core/DiscordErrorCode.cs
    
    Co-authored-by: Discord.Net Robot <robot@elons.sweatshop.stop>
    Co-authored-by: Quin Lynch <49576606+quinchs@users.noreply.github.com>

--- src/Discord.Net.Core/DiscordErrorCode.cs
+++ src/Discord.Net.Core/DiscordErrorCode.cs
@@ -58,6 +58,7 @@ namespace Discord
         #endregion
 
         #region General Actions (20XXX)
+        UnknownTag = 10087,
         BotsCannotUse = 20001,
         OnlyBotsCanUse = 20002,
         CannotSendExplicitContent = 20009,
@@ -98,6 +99,8 @@ namespace Discord
 
         #region General Request Errors (40XXX)
         MaximumNumberOfEditsReached = 30046,
+        MaximumNumberOfPinnedThreadsInAForumChannelReached = 30047,
+        MaximumNumberOfTagsInAForumChannelReached = 30048,
         TokenUnauthorized = 40001,
         InvalidVerification = 40002,
         OpeningDMTooFast = 40003,
@@ -112,6 +115,7 @@ namespace Discord
 
         #region Action Preconditions/Checks (50XXX)
         InteractionHasAlreadyBeenAcknowledged = 40060,
+        TagNamesMustBeUnique = 40061,
         MissingPermissions = 50001,
         InvalidAccountType = 50002,
         CannotExecuteForDM = 50003,
