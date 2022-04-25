8d674fe4faf985b117f143fae3877a1698170ad2
Cenngo <cenk.ergen1@gmail.com>
2022-04-25 21:12:06 +0300

    remove unnecessary usings

--- src/Discord.Net.Core/RequestOptions.cs
+++ src/Discord.Net.Core/RequestOptions.cs
@@ -70,7 +70,6 @@ namespace Discord
         internal bool IsClientBucket { get; set; }
         internal bool IsReactionBucket { get; set; }
         internal bool IsGatewayBucket { get; set; }
-
         internal IDictionary<string, IEnumerable<string?>> RequestHeaders { get; }
 
         internal static RequestOptions CreateOrClone(RequestOptions options)
