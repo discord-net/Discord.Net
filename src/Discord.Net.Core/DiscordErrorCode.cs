9c1200f92f877b0e752592c4b36d5aa644967aa4
Discord.Net Robot <robot@elons.sweatshop.stop>
2022-04-29 19:34:27 +0000

    Add 50080 Error code

--- src/Discord.Net.Core/DiscordErrorCode.cs
+++ src/Discord.Net.Core/DiscordErrorCode.cs
@@ -143,6 +143,7 @@ namespace Discord
         InvalidGuild = 50055,
         PaymentSourceRequiredForGift = 50070,
         CannotDeleteRequiredCommunityChannel = 50074,
+        CannotEditStickersWithinAMessage = 50080,
         InvalidSticker = 50081,
         CannotExecuteOnArchivedThread = 50083,
         InvalidThreadNotificationSettings = 50084,
