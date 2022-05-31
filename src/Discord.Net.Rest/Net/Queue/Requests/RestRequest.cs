b5f2120b1bdb98c0638194b280acb53801cfa179
Cenk Ergen <57065323+Cenngo@users.noreply.github.com>
2022-05-31 17:24:05 +0300

    Add per-request header support to Rest requests (#2261)
    * add support for per-request headers
    
    * remove unnecessary usings

--- src/Discord.Net.Rest/Net/Queue/Requests/RestRequest.cs
+++ src/Discord.Net.Rest/Net/Queue/Requests/RestRequest.cs
@@ -28,7 +28,7 @@ namespace Discord.Net.Queue
 
         public virtual async Task<RestResponse> SendAsync()
         {
-            return await Client.SendAsync(Method, Endpoint, Options.CancelToken, Options.HeaderOnly, Options.AuditLogReason).ConfigureAwait(false);
+            return await Client.SendAsync(Method, Endpoint, Options.CancelToken, Options.HeaderOnly, Options.AuditLogReason, Options.RequestHeaders).ConfigureAwait(false);
         }
     }
 }
