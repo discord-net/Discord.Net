185c7ac22e8e087a85f82d84e79c565786a4ee39
cat <lumitydev@gmail.com>
2022-07-10 23:32:23 -0500

    Support the WEBHOOKS_UPDATED event

---
+++ src/Discord.Net.WebSocket/API/Gateway/WebhooksUpdatedEvent.cs
@@ -0,0 +1,12 @@
+using Newtonsoft.Json;
+
+namespace Discord.API.Gateway;
+
+internal class WebhooksUpdatedEvent
+{
+    [JsonProperty("guild_id")]
+    public ulong GuildId { get; set; }
+
+    [JsonProperty("channel_id")]
+    public ulong ChanelId { get; set; }
+}
