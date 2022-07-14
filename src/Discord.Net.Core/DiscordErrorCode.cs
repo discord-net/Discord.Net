b646815d988a9f89ca4e9b443258273ad6bc2df3
Discord.Net Robot <robot@elons.sweatshop.stop>
2022-07-14 21:43:14 +0000

    Add 20024, 30032, 30052, 40043, 50017, 50132, 50146, 220003 Error codes

--- src/Discord.Net.Core/DiscordErrorCode.cs
+++ src/Discord.Net.Core/DiscordErrorCode.cs
@@ -64,6 +64,7 @@ namespace Discord
         ActionSlowmode = 20016,
         OnlyOwnerAction = 20018,
         AnnouncementEditRatelimit = 20022,
+        UnderMinimumAge = 20024,
         ChannelWriteRatelimit = 20028,
         WriteRatelimitReached = 20029,
         WordsNotAllowed = 20031,
@@ -86,6 +87,7 @@ namespace Discord
         MaximumServerMembersReached = 30019,
         MaximumServerCategoriesReached = 30030,
         GuildTemplateAlreadyExists = 30031,
+        MaximumNumberOfApplicationCommandsReached = 30032,
         MaximumThreadMembersReached = 30033,
         MaximumBansForNonGuildMembersReached = 30035,
         MaximumBanFetchesReached = 30037,
@@ -96,6 +98,7 @@ namespace Discord
         #endregion
 
         #region General Request Errors (40XXX)
+        BitrateIsTooHighForChannelOfThisType = 30052,
         TokenUnauthorized = 40001,
         InvalidVerification = 40002,
         OpeningDMTooFast = 40003,
@@ -108,6 +111,7 @@ namespace Discord
         #endregion
 
         #region Action Preconditions/Checks (50XXX)
+        ApplicationInteractionFailedToSend = 40043,
         MissingPermissions = 50001,
         InvalidAccountType = 50002,
         CannotExecuteForDM = 50003,
@@ -124,6 +128,7 @@ namespace Discord
         InvalidAuthenticationToken = 50014,
         NoteTooLong = 50015,
         ProvidedMessageDeleteCountOutOfBounds = 50016,
+        InvalidMFALevel = 50017,
         InvalidPinChannel = 50019,
         InvalidInvite = 50020,
         CannotExecuteOnSystemMessage = 50021,
@@ -154,6 +159,8 @@ namespace Discord
         #endregion
 
         #region 2FA (60XXX)
+        OwnershipCannotBeTransferredToABotUser = 50132,
+        UploadedFileNotFound. = 50146,
         Requires2FA = 60003,
         #endregion
 
@@ -196,4 +203,5 @@ namespace Discord
         FailedStageCreation = 180002,
         #endregion
     }
+        WebhooksCanOnlyCreateThreadsInForumChannels = 220003,
 }
