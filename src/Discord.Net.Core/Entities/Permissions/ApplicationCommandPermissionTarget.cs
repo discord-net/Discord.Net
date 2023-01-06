e0beb010ba5747eeaaaf08b6e2fe5c0dcbb4b4f0
Cenngo <cenk.ergen1@gmail.com>
2023-01-06 18:20:31 +0300

    fix oopsie

--- src/Discord.Net.Core/Entities/Permissions/ApplicationCommandPermissionTarget.cs
+++ src/Discord.Net.Core/Entities/Permissions/ApplicationCommandPermissionTarget.cs
@@ -12,10 +12,6 @@ namespace Discord
         /// <summary>
         ///     The target of the permission is a user.
         /// </summary>
-        User = 2,
-        /// <summary>
-        ///     The target of the permission is a channel.
-        /// </summary>
-        Channel = 3
+        User = 2
     }
 }
