37a13a209771db37440112eed052900347959370
Misha133 <mihagribkov133@gmail.com>
2023-08-11 18:46:41 +0300

    a

--- src/Discord.Net.V4.Core/Entities/Activities/ActivityProperties.cs
+++ src/Discord.Net.V4.Core/Entities/Activities/ActivityProperties.cs
@@ -1,50 +1,56 @@
-using System;
+namespace Discord;
 
-namespace Discord
+/// <summary>
+///     Flags for the <see cref="IActivity.Flags"/> property, that are ORd together.
+///     These describe what the activity payload includes.
+/// </summary>
+[Flags]
+public enum ActivityProperties
 {
     /// <summary>
-    ///     Flags for the <see cref="IActivity.Flags"/> property, that are ORd together.
-    ///     These describe what the activity payload includes.
+    ///     Indicates that no actions on this activity can be taken.
     /// </summary>
-    [Flags]
-    public enum ActivityProperties
-    {
-        /// <summary>
-        ///     Indicates that no actions on this activity can be taken.
-        /// </summary>
-        None = 0,
-        Instance = 1,
-        /// <summary>
-        ///     Indicates that this activity can be joined.
-        /// </summary>
-        Join = 0b10,
-        /// <summary>
-        ///     Indicates that this activity can be spectated.
-        /// </summary>
-        Spectate = 0b100,
-        /// <summary>
-        ///     Indicates that a user may request to join an activity.
-        /// </summary>
-        JoinRequest = 0b1000,
-        /// <summary>
-        ///     Indicates that a user can listen along in Spotify.
-        /// </summary>
-        Sync = 0b10000,
-        /// <summary>
-        ///     Indicates that a user can play this song.
-        /// </summary>
-        Play = 0b100000,
-        /// <summary>
-        ///     Indicates that a user is playing an activity in a voice channel with friends.
-        /// </summary>
-        PartyPrivacyFriends = 0b1000000,
-        /// <summary>
-        ///     Indicates that a user is playing an activity in a voice channel.
-        /// </summary>
-        PartyPrivacyVoiceChannel = 0b10000000,
-        /// <summary>
-        ///     Indicates that a user is playing an activity in a voice channel.
-        /// </summary>
-        Embedded = 0b10000000
-    }
+    None = 0,
+
+    Instance = 1 << 0,
+
+    /// <summary>
+    ///     Indicates that this activity can be joined.
+    /// </summary>
+    Join = 1 << 1,
+
+    /// <summary>
+    ///     Indicates that this activity can be spectated.
+    /// </summary>
+    Spectate = 1 << 2,
+
+    /// <summary>
+    ///     Indicates that a user may request to join an activity.
+    /// </summary>
+    JoinRequest = 1 << 3,
+
+    /// <summary>
+    ///     Indicates that a user can listen along in Spotify.
+    /// </summary>
+    Sync = 1 << 4,
+
+    /// <summary>
+    ///     Indicates that a user can play this song.
+    /// </summary>
+    Play = 1 << 5,
+
+    /// <summary>
+    ///     Indicates that a user is playing an activity in a voice channel with friends.
+    /// </summary>
+    PartyPrivacyFriends = 1 << 6,
+
+    /// <summary>
+    ///     Indicates that a user is playing an activity in a voice channel.
+    /// </summary>
+    PartyPrivacyVoiceChannel = 1 << 7,
+
+    /// <summary>
+    ///     Indicates that a user is playing an activity in a voice channel.
+    /// </summary>
+    Embedded = 1 << 8
 }
