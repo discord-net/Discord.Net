81e6448f25e494ffb23ccffe1c72f454202efdfb
Misha133 <mihagribkov133@gmail.com>
2023-09-05 00:19:42 +0300

    `Routes` util & start laying out http client

--- src/Discord.Net.V4.Core/Rest/IRestAPIClient.cs
+++ src/Discord.Net.V4.Core/Rest/IRestAPIClient.cs
@@ -1,6 +1,12 @@
 namespace Discord;
 
-internal interface IRestAPIClient
+public interface IRestApiClient
 {
+    Task<T> GetAsync<T>(string route, BucketInfo bucket, RequestOptions options, CancellationToken token);
 
+    Task<T> DeleteAsync<T>(string route, BucketInfo bucket, RequestOptions options, CancellationToken token);
+
+    Task<T> PostAsync<T>(string route, object payload, BucketInfo bucket, RequestOptions options, CancellationToken token);
+
+    Task<T> PutAsync<T>(string route, object payload, BucketInfo bucket, RequestOptions options, CancellationToken token);
 }
---
+++ src/Discord.Net.V4.Core/Rest/Routes.cs
@@ -0,0 +1,63 @@
+using System.Net;
+
+namespace Discord;
+
+/// <summary>
+///     Generates route strings for Discord API endpoints.
+/// </summary>
+public class Routes
+{
+    public static string GetUrlEncodedQueryParams(params (string, object?)[] args)
+    {
+        if (args.All(x => x.Item2 is null))
+            return string.Empty;
+
+        var paramsString = string.Join("&", args.Where(x => x.Item2 is not null)
+            .Select(x => GetUrlEncodedQueryParam(x.Item1, x.Item2!)));
+
+        return $"?{paramsString}";
+    }
+
+    public static string GetUrlEncodedQueryParam(string key, object value)
+        => $"{key}={WebUtility.UrlEncode(value.ToString())}";
+
+
+    public string GetEncodedQueryParams(params object[] args)
+    {
+        if (args.Length == 0)
+            return string.Empty;
+
+        return $"?{string.Join("&" , args.Select(x => WebUtility.UrlEncode(x.ToString())))}";
+    }
+
+    #region User
+
+    public static string GetUser(ulong userId)
+        => $"{DiscordConfig.APIUrl}users/{userId}";
+
+    public static string CurrentUser
+        => $"{DiscordConfig.APIUrl}users/@me";
+
+    public static string GetCurrentUserGuilds(ulong? before = default, ulong? after = default, int? limit = default, bool? withCounts = default)
+        => $"{DiscordConfig.APIUrl}users/@me/guilds{GetUrlEncodedQueryParams(("before", before),
+            ("after", after),
+            ("limit", limit),
+            ("with_counts", withCounts))}";
+
+    public static string GetCurrentUserGuildMember(ulong guildId)
+        => $"{DiscordConfig.APIUrl}users/@me/guilds/{guildId}/member";
+
+    public static string LeaveGuild(ulong guildId)
+        => $"{DiscordConfig.APIUrl}users/@me/guilds/{guildId}";
+
+    public static string CreateDm
+        => "{DiscordConfig.APIUrl}users/@me/channels";
+
+    public static string GetUserConnections
+        => "{DiscordConfig.APIUrl}users/@me/connections";
+
+    public static string ApplicationRoleConnection(ulong applicationId)
+        => $"{DiscordConfig.APIUrl}users/@me/applications/{applicationId}/role-connection";
+
+    #endregion
+}
