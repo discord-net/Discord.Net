7426837de819a4e2741d02be206b764c8866260d
Misha133 <mihagribkov133@gmail.com>
2023-09-28 10:18:18 +0300

    what a big commit lel, add app sub enums

---
+++ src/Discord.Net.Core/Entities/AppSubscriptions/EntitlementType.cs
@@ -0,0 +1,9 @@
+namespace Discord;
+
+public enum EntitlementTypes
+{
+    /// <summary>
+    ///     The entitlement was purchased as an app subscription.
+    /// </summary>
+    ApplicationSubscription = 8
+}
