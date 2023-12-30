9836d1cc6d5c899115c6ee533b67fbf5b0fcf9ff
Misha133 <mihagribkov133@gmail.com>
2023-12-30 11:58:28 +0300

    syntax sugar - implicit operator on `BucketInfo`

--- src/Discord.Net.V4.Core/Rest/BucketInfo.cs
+++ src/Discord.Net.V4.Core/Rest/BucketInfo.cs
@@ -4,4 +4,6 @@ public readonly struct BucketInfo(ScopeType type, ulong value)
 {
     public readonly ScopeType Type = type;
     public readonly ulong Value = value;
+
+    public static implicit operator BucketInfo((ScopeType type, ulong value) tuple) => new (tuple.type, tuple.value);
 }
