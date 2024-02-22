76067db7ee4d036b4e3ab6363f75d8c14809b18b
Misha133 <mihagribkov133@gmail.com>
2024-02-23 01:02:24 +0300

    bugzzz

--- src/Discord.Net.Rest/API/Common/Interaction.cs
+++ src/Discord.Net.Rest/API/Common/Interaction.cs
@@ -59,5 +59,5 @@ internal class Interaction
     public Optional<InteractionContextType> ContextType { get; set; }
 
     [JsonProperty("app_permissions")]
-    public Optional<GuildPermissions> ApplicationPermissions { get; set; }
+    public Optional<GuildPermission> ApplicationPermissions { get; set; }
 }
