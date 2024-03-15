650ae9db116d4bec53e3274810b98a9fee012792
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-03-15 01:12:19 +0100

    Add relatively ugly hack to improve toc filter functionality.

--- docs/_template/material/public/main.js
+++ docs/_template/material/public/main.js
@@ -1,5 +1,7 @@
-﻿export default {
-    iconLinks: [
+﻿export default
+{
+    iconLinks:
+    [
         {
             icon: 'github',
             href: 'https://github.com/discord-net/Discord.Net',
@@ -15,5 +17,53 @@
             href: 'https://discord.gg/dnet',
             title: 'Discord'
         }
-    ]
+    ],
+    start: () =>
+    {
+        // Ugly hack to improve toc filter.
+        let target = document.getElementById("toc");
+        let config = { attributes: false, childList: true, subtree: true };
+        let observer = new MutationObserver((list) =>
+        {
+            for(const mutation of list)
+            {
+                if(mutation.type === "childList" && mutation.target == target)
+                {
+                    let filter = target.getElementsByClassName("form-control")[0];
+
+                    let filterValue = localStorage.getItem("tocFilter");
+                    let scrollValue = localStorage.getItem("tocScroll");
+
+                    if(filterValue && filterValue !== "")
+                    {
+                        filter.value = filterValue;
+
+                        let inputEvent = new Event("input");
+                        filter.dispatchEvent(inputEvent);
+                    }
+
+                    // Add event to store scroll pos.
+                    let tocDiv = target.getElementsByClassName("flex-fill")[0];
+
+                    tocDiv.addEventListener("scroll", (event) =>
+                    {
+                        if (event.target.scrollTop >= 0)
+                        {
+                            localStorage.setItem("tocScroll", event.target.scrollTop);
+                        }
+                    });
+
+                    if(scrollValue && scrollValue >= 0)
+                    {
+                        tocDiv.scroll(0, scrollValue);
+                    }
+
+                    observer.disconnect();
+                    break;
+                }
+            }
+        });
+
+        observer.observe(target, config);
+    }
 }
