54772e77555a344a8eac5a66f5cdb0813672d123
Bence Szucs <bence.szucs200625@gmail.com>
2024-05-15 18:59:47 -0400

    Adds DeferAsyncTimeCheck to DeferAsync in SocketMessageComponent

--- src/Discord.Net.WebSocket/Entities/Interaction/MessageComponents/SocketMessageComponent.cs
+++ src/Discord.Net.WebSocket/Entities/Interaction/MessageComponents/SocketMessageComponent.cs
@@ -442,7 +442,7 @@ namespace Discord.WebSocket
         /// <inheritdoc/>
         public override async Task DeferAsync(bool ephemeral = false, RequestOptions options = null)
         {
-            if (!InteractionHelper.CanSendResponse(this))
+            if (!InteractionHelper.CanSendResponse(this) && Discord.DeferAsyncTimeCheck)
                 throw new TimeoutException($"Cannot defer an interaction after {InteractionHelper.ResponseTimeLimit} seconds of no response/acknowledgement");
 
             var response = new API.InteractionResponse
