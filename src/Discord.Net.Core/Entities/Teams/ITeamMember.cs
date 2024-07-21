6e531a2344c654b3438ce78a1a5374264897eb75
Mihail Gribkov <61027276+Misha-133@users.noreply.github.com>
2024-07-21 17:49:00 +0300

    [Feature] Add team member Role property (#2965)

--- src/Discord.Net.Core/Entities/Teams/ITeamMember.cs
+++ src/Discord.Net.Core/Entities/Teams/ITeamMember.cs
@@ -1,25 +1,32 @@
-namespace Discord
+namespace Discord;
+
+/// <summary>
+///     Represents a Discord Team member.
+/// </summary>
+public interface ITeamMember
 {
     /// <summary>
-    ///     Represents a Discord Team member.
+    ///     Gets the membership state of this team member.
     /// </summary>
-    public interface ITeamMember
-    {
-        /// <summary>
-        ///     Gets the membership state of this team member.
-        /// </summary>
-        MembershipState MembershipState { get; }
-        /// <summary>
-        ///     Gets the permissions of this team member.
-        /// </summary>
-        string[] Permissions { get; }
-        /// <summary>
-        ///     Gets the team unique identifier for this team member.
-        /// </summary>
-        ulong TeamId { get; }
-        /// <summary>
-        ///     Gets the Discord user of this team member.
-        /// </summary>
-        IUser User { get; }
-    }
+    MembershipState MembershipState { get; }
+
+    /// <summary>
+    ///     Gets the permissions of this team member.
+    /// </summary>
+    string[] Permissions { get; }
+
+    /// <summary>
+    ///     Gets the team unique identifier for this team member.
+    /// </summary>
+    ulong TeamId { get; }
+
+    /// <summary>
+    ///     Gets the Discord user of this team member.
+    /// </summary>
+    IUser User { get; }
+
+    /// <summary>
+    ///     Gets the role of this team member.
+    /// </summary>
+    public TeamRole Role { get; }
 }
