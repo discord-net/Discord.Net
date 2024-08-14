bc84b94bd903d480093058bf4b56f8ce635dcb66
Adriaan Waem <2ndalgorithm@gmail.com>
2024-08-14 21:57:25 +0200

    Create a (Discord)Color using a string (#2979)
    * Create a (Discord)Color using a string
    
    * You made me learn bitwise operators and shifting I am angery now, also AI sucks at this shit
    
    * xmldoc
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Mihail Gribkov <61027276+Misha-133@users.noreply.github.com>

---
+++ src/Discord.Net.Core/Entities/Roles/ColorType.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Discord;
+
+public enum ColorType
+{
+    /// <summary>
+    ///     Color in one of the following formats.
+    /// <para>
+    ///     #RGB        The three-value syntax
+    /// </para>
+    /// <para>
+    ///     #RGBA       The four-value syntax
+    /// </para>
+    /// <para>
+    ///     #RRGGBB     The six-value syntax
+    /// </para>
+    ///     #RRGGBBAA   The eight-value syntax
+    /// </summary>
+    CssHexColor = 0
+}
