3932fd7046885fd19d7d9e437e2d409311852af5
Zach Goodson <quantamstudios@gmail.com>
2025-03-07 16:34:08 -0600

    Make DiscordShardedClient.GetUserAsync() public

--- src/Discord.Net.WebSocket/DiscordShardedClient.cs
+++ src/Discord.Net.WebSocket/DiscordShardedClient.cs
@@ -611,7 +611,7 @@ namespace Discord.WebSocket
             => await CreateGuildAsync(name, region, jpegIcon).ConfigureAwait(false);
 
         /// <inheritdoc />
-        async Task<IUser> IDiscordClient.GetUserAsync(ulong id, CacheMode mode, RequestOptions options)
+        public async Task<IUser> GetUserAsync(ulong id, CacheMode mode, RequestOptions options)
         {
             var user = GetUser(id);
             if (user is not null || mode == CacheMode.CacheOnly)
