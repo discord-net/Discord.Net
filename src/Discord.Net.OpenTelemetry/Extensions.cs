05f597f7a395f5b14c1e1ef66248d5102d5801d3
Suiram1701 <110390261+Suiram1701@users.noreply.github.com>
2025-04-24 20:06:08 +0200

    Added the source names to the extensions and added otel project to the Github actions workflow

--- src/Discord.Net.OpenTelemetry/Extensions.cs
+++ src/Discord.Net.OpenTelemetry/Extensions.cs
@@ -9,14 +9,20 @@ namespace Discord.OpenTelemetry
     /// </summary>
     public static class Extensions
     {
+        private const string SourceName = "Discord.Net.WebSocket";
+
         public static TracerProviderBuilder AddDiscordNetInstrumentation(this TracerProviderBuilder builder)
         {
-            throw new NotImplementedException();
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+            return builder.AddSource(SourceName);
         }
 
         public static MeterProviderBuilder AddDiscordNetInstrumentation(this MeterProviderBuilder builder)
         {
-            throw new NotImplementedException();
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+            return builder.AddMeter(SourceName);
         }
     }
 
