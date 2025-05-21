7b86186fd463e63d0b6edb15b22ee6c537f497d4
OverwrittenCode <kylethapar2007@outlook.com>
2025-05-21 13:01:39 +0100

    refactor(TimestampTagStyles): replace ASCII integer values with character literals (#1)

--- src/Discord.Net.Core/Entities/Messages/TimestampTagStyle.cs
+++ src/Discord.Net.Core/Entities/Messages/TimestampTagStyle.cs
@@ -8,36 +8,36 @@ namespace Discord
         /// <summary>
         ///     A short time string: 16:20
         /// </summary>
-        ShortTime = 116,
+        ShortTime = 't',
 
         /// <summary>
         ///     A long time string: 16:20:30
         /// </summary>
-        LongTime = 84,
+        LongTime = 'T',
 
         /// <summary>
         ///     A short date string: 20/04/2021
         /// </summary>
-        ShortDate = 100,
+        ShortDate = 'd',
 
         /// <summary>
         ///     A long date string: 20 April 2021
         /// </summary>
-        LongDate = 68,
+        LongDate = 'D',
 
         /// <summary>
         ///     A short datetime string: 20 April 2021 16:20
         /// </summary>
-        ShortDateTime = 102,
+        ShortDateTime = 'f',
 
         /// <summary>
         ///     A long datetime string: Tuesday, 20 April 2021 16:20
         /// </summary>
-        LongDateTime = 70,
+        LongDateTime = 'F',
 
         /// <summary>
         ///     The relative time to the user: 2 months ago
         /// </summary>
-        Relative = 82
+        Relative = 'R'
     }
 }
