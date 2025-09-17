6c50876c39af2fcc9e2f7e4831a4fe06e34f9d84
Cenk Ergen <cenk.ergen1@gmail.com>
2025-09-18 01:05:42 +0200

    add write logic to the nullable modal component typeconverter

--- src/Discord.Net.Interactions/TypeConverters/ModalComponents/NullableModalComponentConverter.cs
+++ src/Discord.Net.Interactions/TypeConverters/ModalComponents/NullableModalComponentConverter.cs
@@ -20,4 +20,7 @@ internal class NullableModalComponentConverter<T> : ModalComponentTypeConverter<
 
     public override Task<TypeConverterResult> ReadAsync(IInteractionContext context, IComponentInteractionData option, IServiceProvider services)
         => string.IsNullOrEmpty(option.Value) ? Task.FromResult(TypeConverterResult.FromSuccess(null)) : _typeConverter.ReadAsync(context, option, services);
+
+    public override Task WriteAsync<TBuilder>(TBuilder builder, InputComponentInfo component, object value)
+        => _typeConverter.WriteAsync(builder, component, value);
 }
