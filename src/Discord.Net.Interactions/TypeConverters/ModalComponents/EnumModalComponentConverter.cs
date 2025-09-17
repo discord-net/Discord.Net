1de62c073157009def5c9503145ca2f1d3f4afee
Cenk Ergen <cenk.ergen1@gmail.com>
2025-09-18 00:58:52 +0200

    add read logic to enum modal component typeconverter

--- src/Discord.Net.Interactions/TypeConverters/ModalComponents/EnumModalComponentConverter.cs
+++ src/Discord.Net.Interactions/TypeConverters/ModalComponents/EnumModalComponentConverter.cs
@@ -10,6 +10,7 @@ namespace Discord.Interactions.TypeConverters.ModalComponents;
 internal sealed class EnumModalComponentConverter<T> : ModalComponentTypeConverter<T>
     where T : struct, Enum
 {
+    private readonly bool _isFlags;
     private readonly ImmutableArray<SelectMenuOptionBuilder> _options;
 
     public EnumModalComponentConverter()
@@ -20,6 +21,8 @@ internal sealed class EnumModalComponentConverter<T> : ModalComponentTypeConvert
         if (members.Count() > SelectMenuBuilder.MaxOptionCount)
             throw new InvalidOperationException($"Enum type {typeof(T).FullName} has too many visible members to be used in a select menu. Maximum visible members is {SelectMenuBuilder.MaxOptionCount}, but {members.Count()} are visible.");
 
+        _isFlags = typeof(T).GetCustomAttribute<FlagsAttribute>() is not null;
+
         _options = members.Select(x =>
         {
             var selectMenuOptionAttr = x.GetCustomAttribute<SelectMenuOptionAttribute>();
@@ -27,12 +30,31 @@ internal sealed class EnumModalComponentConverter<T> : ModalComponentTypeConvert
         }).ToImmutableArray();
     }
 
-    public override Task<TypeConverterResult> ReadAsync(IInteractionContext context, IComponentInteractionData option, IServiceProvider services) => throw new NotImplementedException();
+    public override Task<TypeConverterResult> ReadAsync(IInteractionContext context, IComponentInteractionData option, IServiceProvider services)
+    {
+        if(option.Type is not ComponentType.SelectMenu or ComponentType.TextInput)
+            return Task.FromResult(TypeConverterResult.FromError(InteractionCommandError.ConvertFailed, $"{option.Type} input type cannot be converted to {typeof(T).FullName}"));
+
+        var value = option.Type switch
+        {
+            ComponentType.SelectMenu => string.Join(",", option.Values),
+            ComponentType.TextInput => option.Value,
+            _ => null
+        };
+
+        if(Enum.TryParse<T>(value, out var result))
+            return Task.FromResult(TypeConverterResult.FromSuccess(result));
+
+        return Task.FromResult(TypeConverterResult.FromError(InteractionCommandError.ConvertFailed, $"Value {option.Value} cannot be converted to {typeof(T).FullName}"));
+    }
 
     public override Task WriteAsync<TBuilder>(TBuilder builder, InputComponentInfo component, object value)
     {
         if (builder is not SelectMenuBuilder selectMenu || component.ComponentType is not ComponentType.SelectMenu)
             throw new InvalidOperationException($"{nameof(EnumModalComponentConverter<T>)} can only write to select menu components.");
+
+        if(selectMenu.MaxValues > 1 && !_isFlags)
+            throw new InvalidOperationException($"Enum type {typeof(T).FullName} is not a [Flags] enum, so it cannot be used in a multi-select menu.");
 
         selectMenu.WithOptions(_options.ToList());
 
