0ae42e0acca47a8424fbe8b118a23cfbb28b8a31
Cenk Ergen <cenk.ergen1@gmail.com>
2025-11-10 10:08:07 +0100

    rename select input attribute

---
+++ src/Discord.Net.Interactions/Attributes/Modals/ModalSelectInputAttribute.cs
@@ -0,0 +1,28 @@
+namespace Discord.Interactions;
+
+/// <summary>
+///     Base attribute for select-menu, user, channel, role, and mentionable select inputs in modals.
+/// </summary>
+public abstract class ModalSelectInputAttribute : ModalInputAttribute
+{
+    /// <summary>
+    ///     Gets or sets the minimum number of values that can be selected.
+    /// </summary>
+    public int MinValues { get; set; } = 1;
+
+    /// <summary>
+    ///     Gets or sets the maximum number of values that can be selected.
+    /// </summary>
+    public int MaxValues { get; set; } = 1;
+
+    /// <summary>
+    ///     Gets or sets the placeholder text.
+    /// </summary>
+    public string Placeholder { get; set; }
+
+    internal ModalSelectInputAttribute(string customId, int minValues = 1, int maxValues = 1) : base(customId)
+    {
+        MinValues = minValues;
+        MaxValues = maxValues;
+    }
+}
