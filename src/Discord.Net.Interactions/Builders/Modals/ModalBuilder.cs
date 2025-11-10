120ab0472e449b6085dcabad184cb1082d513681
Cenk Ergen <cenk.ergen1@gmail.com>
2025-11-10 10:10:40 +0100

    add inline docs to modalBuilder

--- src/Discord.Net.Interactions/Builders/Modals/ModalBuilder.cs
+++ src/Discord.Net.Interactions/Builders/Modals/ModalBuilder.cs
@@ -1,132 +1,158 @@
-using Discord.Interactions.Builders.Modals.Inputs;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace Discord.Interactions.Builders
+namespace Discord.Interactions.Builders;
+
+/// <summary>
+///     Represents a builder for creating <see cref="ModalInfo"/>.
+/// </summary>
+public class ModalBuilder
 {
+    internal readonly InteractionService _interactionService;
+    internal readonly List<IInputComponentBuilder> _components;
+
     /// <summary>
-    ///     Represents a builder for creating <see cref="ModalInfo"/>.
+    ///     Gets the initialization delegate for this modal.
     /// </summary>
-    public class ModalBuilder
+    public ModalInitializer ModalInitializer { get; internal set; }
+
+    /// <summary>
+    ///     Gets the title of this modal.
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    ///     Gets the <see cref="IModal"/> implementation used to initialize this object.
+    /// </summary>
+    public Type Type { get; }
+
+    /// <summary>
+    ///     Gets a collection of the components of this modal.
+    /// </summary>
+    public IReadOnlyCollection<IInputComponentBuilder> Components => _components;
+
+    internal ModalBuilder(Type type, InteractionService interactionService)
     {
-        internal readonly InteractionService _interactionService;
-        internal readonly List<IInputComponentBuilder> _components;
+        if (!typeof(IModal).IsAssignableFrom(type))
+            throw new ArgumentException($"Must be an implementation of {nameof(IModal)}", nameof(type));
 
-        /// <summary>
-        ///     Gets the initialization delegate for this modal.
-        /// </summary>
-        public ModalInitializer ModalInitializer { get; internal set; }
+        Type = type;
 
-        /// <summary>
-        ///     Gets the title of this modal.
-        /// </summary>
-        public string Title { get; set; }
+        _interactionService = interactionService;
+        _components = new();
+    }
 
-        /// <summary>
-        ///     Gets the <see cref="IModal"/> implementation used to initialize this object.
-        /// </summary>
-        public Type Type { get; }
+    /// <summary>
+    ///     Initializes a new <see cref="ModalBuilder"/>
+    /// </summary>
+    /// <param name="modalInitializer">The initialization delegate for this modal.</param>
+    public ModalBuilder(Type type, ModalInitializer modalInitializer, InteractionService interactionService) : this(type, interactionService)
+    {
+        ModalInitializer = modalInitializer;
+    }
 
-        /// <summary>
-        ///     Gets a collection of the components of this modal.
-        /// </summary>
-        public IReadOnlyCollection<IInputComponentBuilder> Components => _components;
+    /// <summary>
+    ///     Sets <see cref="Title"/>.
+    /// </summary>
+    /// <param name="title">New value of the <see cref="Title"/>.</param>
+    /// <returns>
+    ///     The builder instance.
+    /// </returns>
+    public ModalBuilder WithTitle(string title)
+    {
+        Title = title;
+        return this;
+    }
 
-        internal ModalBuilder(Type type, InteractionService interactionService)
-        {
-            if (!typeof(IModal).IsAssignableFrom(type))
-                throw new ArgumentException($"Must be an implementation of {nameof(IModal)}", nameof(type));
+    /// <summary>
+    ///     Adds text components to <see cref="Components"/>.
+    /// </summary>
+    /// <param name="configure">Text Component builder factory.</param>
+    /// <returns>
+    ///     The builder instance.
+    /// </returns>
+    public ModalBuilder AddTextComponent(Action<TextInputComponentBuilder> configure)
+    {
+        var builder = new TextInputComponentBuilder(this);
+        configure(builder);
+        _components.Add(builder);
+        return this;
+    }
 
-            Type = type;
+    /// <summary>
+    ///     Adds a select menu component to <see cref="Components"/>.
+    /// </summary>
+    /// <param name="configure">Select menu component builder factory.</param>
+    /// <returns>
+    ///     The builder instance.
+    /// </returns>
+    public ModalBuilder AddSelectMenuComponent(Action<SelectMenuInputComponentBuilder> configure)
+    {
+        var builder = new SelectMenuInputComponentBuilder(this);
+        configure(builder);
+        _components.Add(builder);
+        return this;
+    }
 
-            _interactionService = interactionService;
-            _components = new();
-        }
+    /// <summary>
+    ///     Adds a user select component to <see cref="Components"/>.
+    /// </summary>
+    /// <param name="configure">User select component builder factory.</param>
+    /// <returns>
+    ///     The builder instance.
+    /// </returns>
+    public ModalBuilder AddUserSelectComponent(Action<UserSelectInputComponentBuilder> configure)
+    {
+        var builder = new UserSelectInputComponentBuilder(this);
+        configure(builder);
+        _components.Add(builder);
+        return this;
+    }
 
-        /// <summary>
-        ///     Initializes a new <see cref="ModalBuilder"/>
-        /// </summary>
-        /// <param name="modalInitializer">The initialization delegate for this modal.</param>
-        public ModalBuilder(Type type, ModalInitializer modalInitializer, InteractionService interactionService) : this(type, interactionService)
-        {
-            ModalInitializer = modalInitializer;
-        }
+    /// <summary>
+    ///     Adds a role select component to <see cref="Components"/>.
+    /// </summary>
+    /// <param name="configure">Role select component builder factory.</param>
+    /// <returns>
+    ///     The builder instance.
+    /// </returns>
+    public ModalBuilder AddRoleSelectComponent(Action<RoleSelectInputComponentBuilder> configure)
+    {
+        var builder = new RoleSelectInputComponentBuilder(this);
+        configure(builder);
+        _components.Add(builder);
+        return this;
+    }
 
-        /// <summary>
-        ///     Sets <see cref="Title"/>.
-        /// </summary>
-        /// <param name="title">New value of the <see cref="Title"/>.</param>
-        /// <returns>
-        ///     The builder instance.
-        /// </returns>
-        public ModalBuilder WithTitle(string title)
-        {
-            Title = title;
-            return this;
-        }
+    /// <summary>
+    ///     Adds a mentionable select component to <see cref="Components"/>.
+    /// </summary>
+    /// <param name="configure">Mentionable select component builder factory.</param>
+    /// <returns>
+    ///     The builder instance.
+    /// </returns>
+    public ModalBuilder AddMentionableSelectComponent(Action<MentionableSelectInputComponentBuilder> configure)
+    {
+        var builder = new MentionableSelectInputComponentBuilder(this);
+        configure(builder);
+        _components.Add(builder);
+        return this;
+    }
 
-        /// <summary>
-        ///     Adds text components to <see cref="Components"/>.
-        /// </summary>
-        /// <param name="configure">Text Component builder factory.</param>
-        /// <returns>
-        ///     The builder instance.
-        /// </returns>
-        public ModalBuilder AddTextComponent(Action<TextInputComponentBuilder> configure)
-        {
-            var builder = new TextInputComponentBuilder(this);
-            configure(builder);
-            _components.Add(builder);
-            return this;
-        }
-
-        /// <summary>
-        ///     Adds a select menu component to <see cref="Components"/>.
-        /// </summary>
-        /// <param name="configure">Select menu component builder factory.</param>
-        /// <returns>
-        ///     The builder instance.
-        /// </returns>
-        public ModalBuilder AddSelectMenuComponent(Action<SelectMenuInputComponentBuilder> configure)
-        {
-            var builder = new SelectMenuInputComponentBuilder(this);
-            configure(builder);
-            _components.Add(builder);
-            return this;
-        }
-
-        public ModalBuilder AddUserSelectComponent(Action<UserSelectInputComponentBuilder> configure)
-        {
-            var builder = new UserSelectInputComponentBuilder(this);
-            configure(builder);
-            _components.Add(builder);
-            return this;
-        }
-
-        public ModalBuilder AddRoleSelectComponent(Action<RoleSelectInputComponentBuilder> configure)
-        {
-            var builder = new RoleSelectInputComponentBuilder(this);
-            configure(builder);
-            _components.Add(builder);
-            return this;
-        }
-
-        public ModalBuilder AddMentionableSelectComponent(Action<MentionableSelectInputComponentBuilder> configure)
-        {
-            var builder = new MentionableSelectInputComponentBuilder(this);
-            configure(builder);
-            _components.Add(builder);
-            return this;
-        }
-
-        public ModalBuilder AddChannelSelectComponent(Action<ChannelSelectInputComponentBuilder> configure)
-        {
-            var builder = new ChannelSelectInputComponentBuilder(this);
-            configure(builder);
-            _components.Add(builder);
-            return this;
-        }
+    /// <summary>
+    ///     Adds a channel select component to <see cref="Components"/>.
+    /// </summary>
+    /// <param name="configure">Channel select component builder factory.</param>
+    /// <returns>
+    ///     The builder instance.
+    /// </returns>
+    public ModalBuilder AddChannelSelectComponent(Action<ChannelSelectInputComponentBuilder> configure)
+    {
+        var builder = new ChannelSelectInputComponentBuilder(this);
+        configure(builder);
+        _components.Add(builder);
+        return this;
+    }
 
     /// <summary>
     ///     Adds a file upload component to <see cref="Components"/>.
@@ -143,6 +169,5 @@ namespace Discord.Interactions.Builders
         return this;
     }
 
-        internal ModalInfo Build() => new(this);
-    }
+    internal ModalInfo Build() => new(this);
 }
