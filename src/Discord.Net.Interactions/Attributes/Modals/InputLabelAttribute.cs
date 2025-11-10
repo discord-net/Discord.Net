741aa51debd60cb12d44e89d1e88c2310524e47e
Cenk Ergen <cenk.ergen1@gmail.com>
2025-11-10 09:58:22 +0100

    add description property to input label attribute

--- src/Discord.Net.Interactions/Attributes/Modals/InputLabelAttribute.cs
+++ src/Discord.Net.Interactions/Attributes/Modals/InputLabelAttribute.cs
@@ -1,25 +1,31 @@
 using System;
 
-namespace Discord.Interactions
+namespace Discord.Interactions;
+
+/// <summary>
+///     Creates a custom label for an modal input.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class InputLabelAttribute : Attribute
 {
+    /// <summary>
+    ///     Gets the label of the input.
+    /// </summary>
+    public string Label { get; }
+
+    /// <summary>
+    ///     Gets the label description of the input.
+    /// </summary>
+    public string Description { get; set; }
+
     /// <summary>
     ///     Creates a custom label for an modal input.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-    public class InputLabelAttribute : Attribute
+    /// <param name="label">The label of the input.</param>
+    /// <param name="description">The label description of the input.</param>
+    public InputLabelAttribute(string label, string description = null)
     {
-        /// <summary>
-        ///     Gets the label of the input.
-        /// </summary>
-        public string Label { get; }
-
-        /// <summary>
-        ///     Creates a custom label for an modal input.
-        /// </summary>
-        /// <param name="label">The label of the input.</param>
-        public InputLabelAttribute(string label)
-        {
-            Label = label;
-        }
+        Label = label;
+        Description = description;
     }
 }
