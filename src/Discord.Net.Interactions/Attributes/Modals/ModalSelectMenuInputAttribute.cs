a4518f1ff4c6ea5f78b13f55250f82f5c87d204f
Cenk Ergen <cenk.ergen1@gmail.com>
2025-11-12 01:06:33 +0100

    add default min/max values to select input attribute

--- src/Discord.Net.Interactions/Attributes/Modals/ModalSelectMenuInputAttribute.cs
+++ src/Discord.Net.Interactions/Attributes/Modals/ModalSelectMenuInputAttribute.cs
@@ -14,7 +14,7 @@ public sealed class ModalSelectMenuInputAttribute : ModalSelectInputAttribute
     /// <param name="customId">Custom ID of the select menu component.</param>
     /// <param name="minValues">Minimum number of values that can be selected.</param>
     /// <param name="maxValues">Maximum number of values that can be selected.</param>
-    public ModalSelectMenuInputAttribute(string customId, int minValues, int maxValues) : base(customId)
+    public ModalSelectMenuInputAttribute(string customId, int minValues = 1, int maxValues = 1) : base(customId, minValues, maxValues)
     {
     }
 }
