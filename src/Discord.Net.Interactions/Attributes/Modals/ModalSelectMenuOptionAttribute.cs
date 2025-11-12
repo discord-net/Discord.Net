751a71dc331ea9c5bc129fd34bf2dc9a38881b78
Cenk Ergen <cenk.ergen1@gmail.com>
2025-11-12 01:13:08 +0100

    add modal select menu option attribute

---
+++ src/Discord.Net.Interactions/Attributes/Modals/ModalSelectMenuOptionAttribute.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace Discord.Interactions.Attributes.Modals;
+
+/// <summary>
+///     Adds a select menu option to the marked field.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+public class ModalSelectMenuOptionAttribute : Attribute
+{
+    /// <summary>
+    ///     Gets the label of the option.
+    /// </summary>
+    public string Label { get; }
+
+    /// <summary>
+    ///     Gets or sets the description of the option.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    ///     Gets the value of the option.
+    /// </summary>
+    public string Value { get; }
+
+    /// <summary>
+    ///     Gets or sets the emote of the option.
+    /// </summary>
+    public string Emote { get; set; }
+
+    /// <summary>
+    ///     Gets or sets whether the option is selected by default.
+    /// </summary>
+    public bool IsDefault { get; set; }
+
+    /// <summary>
+    ///     Create a new <see cref="ModalSelectInputAttribute"/>.
+    /// </summary>
+    /// <param name="label">Label of the option.</param>
+    /// <param name="value">Value of the option.</param>
+    /// <param name="description">Description of the option.</param>
+    /// <param name="emote">Emote of the option.</param>
+    /// <param name="isDefault">Whether the option is selected by default</param>
+    public ModalSelectMenuOptionAttribute(string label, string value, string description = null, string emote = null, bool isDefault = false)
+    {
+        Label = label;
+        Value = value;
+        Description = description;
+        Emote = emote;
+        IsDefault = isDefault;
+    }
+}
