3e5e1962d4cf0a60a7f23be016843dc11145ac7c
Cenk Ergen <cenk.ergen1@gmail.com>
2025-11-12 01:12:58 +0100

    add text display attribute

---
+++ src/Discord.Net.Interactions/Attributes/Modals/ModalTextDisplayAttribute.cs
@@ -0,0 +1,26 @@
+using Discord.Interactions.Attributes.Modals;
+
+namespace Discord.Interactions;
+
+/// <summary>
+///     Marks a <see cref="IModal"/> property as a text input.
+/// </summary>
+public class ModalTextDisplayAttribute : ModalComponentAttribute
+{
+    /// <inheritdoc/>
+    public override ComponentType ComponentType => ComponentType.TextDisplay;
+
+    /// <summary>
+    ///     Gets the content of the text display.
+    /// </summary>
+    public string Content { get; }
+
+    /// <summary>
+    ///     Create a new <see cref="ModalTextInputAttribute"/>.
+    /// </summary>
+    /// <param name="content">Content of the text display.</param>
+    public ModalTextDisplayAttribute(string content = null)
+    {
+        Content = content;
+    }
+}
