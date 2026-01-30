42473602318ddff3cdc79fc010f90b462a47e181
Cenk Ergen <cenk.ergen1@gmail.com>
2026-01-30 23:12:23 +0100

    add option attributes for checkbox group and radio group components

---
+++ src/Discord.Net.Interactions/Attributes/Modals/ModalCheckboxGroupOptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Discord.Interactions;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+public class ModalCheckboxGroupOptionAttribute : Attribute
+{
+    public string Value { get; set; }
+
+    public string Label { get; set; }
+
+    public string Description { get; set; }
+
+    public bool DefaultState { get; set; }
+
+    public ModalCheckboxGroupOptionAttribute(string value, string label, string description = null, bool defaultState = false)
+    {
+        Value = value;
+        Label = label;
+        Description = description;
+        DefaultState = defaultState;
+    }
+}
---
+++ src/Discord.Net.Interactions/Attributes/Modals/ModalRadioGroupOptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Discord.Interactions;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+public class ModalRadioGroupOptionAttribute : Attribute
+{
+    public string Value { get; set; }
+
+    public string Label { get; set; }
+
+    public string Description { get; set; }
+
+    public bool IsDefault { get; set; }
+
+    public ModalRadioGroupOptionAttribute(string value, string label, string description = null, bool isDefault = false)
+    {
+        Value = value;
+        Label = label;
+        Description = description;
+        IsDefault = isDefault;
+    }
+}
