e4f827808215732b188a187bb77cfc9b3ce2f3e1
Cenk Ergen <cenk.ergen1@gmail.com>
2026-01-30 23:10:20 +0100

    add checkbox group component attribute

---
+++ src/Discord.Net.Interactions/Attributes/Modals/ModalCheckboxGroupAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Discord.Interactions;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class ModalCheckboxGroupAttribute : ModalInputAttribute
+{
+    public override ComponentType ComponentType => ComponentType.CheckboxGroup;
+
+    public int MinValues { get; set; } = 1;
+
+    public int MaxValues { get; set; } = 1;
+
+    public ModalCheckboxGroupAttribute(string customId, int id = 0, int minValues = 1, int maxValues = 1)
+        : base(customId, id)
+    {
+        MinValues = minValues;
+        MaxValues = maxValues;
+    }
+}
