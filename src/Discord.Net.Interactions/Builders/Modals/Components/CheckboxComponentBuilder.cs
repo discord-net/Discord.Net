2e64c15156696251700e206c1cafb25c5b66a38b
Cenk Ergen <cenk.ergen1@gmail.com>
2026-01-30 23:07:57 +0100

    create builder and info entities for checkbox component

---
+++ src/Discord.Net.Interactions/Builders/Modals/Components/CheckboxComponentBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Discord.Interactions.Builders;
+
+public class CheckboxComponentBuilder : InputComponentBuilder<CheckboxComponentInfo, CheckboxComponentBuilder>
+{
+    protected override CheckboxComponentBuilder Instance => this;
+
+    public bool DefaultState { get; set; } = false;
+
+    internal CheckboxComponentBuilder(ModalBuilder modal) : base(modal)
+    {
+    }
+
+    public CheckboxComponentBuilder WithDefaultState(bool defaultState)
+    {
+        DefaultState = defaultState;
+        return this;
+    }
+
+    internal override CheckboxComponentInfo Build(ModalInfo modal) => new(this, modal);
+}
