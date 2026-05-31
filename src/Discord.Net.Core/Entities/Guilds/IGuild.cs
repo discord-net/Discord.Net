7fd01f461481a8bd83089ca60d1265e150148ef1
Misha133 <61027276+Misha-133@users.noreply.github.com>
2026-05-31 18:11:51 +0300

    paramref

--- src/Discord.Net.Core/Entities/Guilds/IGuild.cs
+++ src/Discord.Net.Core/Entities/Guilds/IGuild.cs
@@ -1468,7 +1468,7 @@ namespace Discord
         /// <param name="guildMemberCacheMode">The cache mode to use when retrieving guild members.</param>
         /// <param name="options">The options to be used when sending the request.</param>
         /// <remarks>
-        ///     When using <see cref="guildMemberCacheMode"/> set to <see cref="CacheMode.CacheOnly"/>, the author of retrieved messages will be a cached <see cref="IGuildUser"/>
+        ///     When using <paramref name="guildMemberCacheMode"/> set to <see cref="CacheMode.CacheOnly"/>, the author of retrieved messages will be a cached <see cref="IGuildUser"/>
         ///     or <see cref="IUser"/> constructed from the retrieved data. When using <see cref="CacheMode.AllowDownload"/>, the author will be a full <see cref="IGuildUser"/> retrieved from the API,
         ///     which may trigger additional requests if the user is not cached.
         /// </remarks>
